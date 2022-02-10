75cefde69999397782b081b91c779bfaa34e3708
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-02-10 12:21:22 +0200

    Add files via upload
    Refractoring of Stokes solvers and added Solvi, SolKz and SolCx benchmarks

--- src/JustRelax.jl
+++ src/JustRelax.jl
@@ -1,5 +1,201 @@
 module JustRelax
 
-include("DiffusionNonlinearSolvers.jl")
+# include("DiffusionNonlinearSolvers.jl")
+
+using ParallelStencil
+using LinearAlgebra
+using Printf
+
+
+# PS.jl exports
+# import ParallelStencil: @parallel, @hide_communication, @parallel_indices, @parallel_async, @synchronize, @zeros, @ones, @rand
+# export @parallel, @hide_communication, @parallel_indices, @parallel_async, @synchronize, @zeros, @ones, @rand
+
+export PS_Setup, Geometry
+
+export environment!, PS_reset!
+struct PS_Setup{B,C}
+    device::Symbol
+
+    function PS_Setup(device::Symbol, precission::DataType, nDim::Integer)
+        new{precission, nDim}(device)
+    end
+end
+
+struct Geometry{nDim}
+    ni::NTuple{nDim, Integer}
+    li::NTuple{nDim, Float64}
+    max_li::Float64
+    di::NTuple{nDim, Float64}
+    xci::NTuple{nDim, StepRangeLen{Float64, Base.TwicePrecision{Float64}, Base.TwicePrecision{Float64}, Int64}}
+    xvi::NTuple{nDim, StepRangeLen{Float64, Base.TwicePrecision{Float64}, Base.TwicePrecision{Float64}, Int64}}
+
+    function Geometry(ni::NTuple{nDim, Integer}, li::NTuple{nDim, T}) where {nDim, T}
+        li isa NTuple{nDim, Float64} == false && (li = Float64.(li))
+        di = li./ni
+        new{nDim}(
+            ni,
+            li,
+            Float64(max(li...)),
+            di,
+            Tuple([di[i]/2:di[i]:(li[i]-di[i]/2) for i in 1:nDim]),
+            Tuple([0:di[i]:li[i] for i in 1:nDim])
+        )
+    end
+
+end
+
+function environment!(model::PS_Setup{T, N}) where {T, N}
+    gpu = model.device == :gpu ? true : false
+
+    # environment variable for XPU
+    @eval(
+        const USE_GPU = haskey(ENV, "USE_GPU"    ) ? parse(Bool, ENV["USE_GPU"]    ) : $gpu
+    )
+    
+    # call appropriate FD module
+    eval(
+        Meta.parse("using ParallelStencil.FiniteDifferences$(N)D")
+    )
+
+    # start ParallelStencil
+    local PTArray
+    if model.device == :gpu 
+        eval(
+            Meta.parse("@init_parallel_stencil(CUDA, $T, $N)")
+        )
+        eval(
+            :(const PTArray =  CUDA.CuArray{$T, $N})
+        )
+    else
+        eval(
+            Meta.parse("@init_parallel_stencil(Threads, $T, $N)")
+        )
+        eval(
+            :(const PTArray = Array{$T, $N})
+        )
+    end
+
+    # create array structs
+    make_velocity_struct!(N) # velocity
+    make_tensor_struct!(N) # (symmetric) tensors
+    make_residual_struct!(N) # residuals
+    make_stokes_struct!() # Arrays for Stokes solver
+    make_PTstokes_struct!()
+
+    eval(
+        :(include("Stokes/Stokes.jl"))
+    )
+
+    eval(
+        :(include("BoundaryConditions/BoundaryConditions.jl"))
+    )
+
+    eval(
+        :(export USE_GPU, PTArray, SymmetricTensor, Residual, StokesArrays, PTStokesCoeffs, pureshear_bc!, smooth!, solve!)
+    )
+
+end
+
+function make_velocity_struct!(nDim::Integer)
+    dims = ("x", "y", "z")
+    str = Meta.parse(
+        "struct Velocity{T} \n"*
+        join("V$(dims[i])::T\n" for i in 1:nDim)*
+        "end"
+    )
+    eval(str)
+end
+
+function make_tensor_struct!(nDim::Integer)
+    dims = ("x", "y", "z")
+    str = Meta.parse(
+        "struct SymmetricTensor{T} \n"*
+        join("$(dims[i])$(dims[j])::T\n" for i in 1:nDim, j in 1:nDim if j≥i)*
+        "end"
+    )
+    eval(str)
+end
+
+function make_residual_struct!(nDim::Integer)
+    dims = ("x", "y", "z")
+    str = Meta.parse(
+        "struct Residual{T} \n"*
+        join("R$(dims[i])::T\n" for i in 1:nDim)*
+        "end"
+    )
+    eval(str)
+end
+
+make_stokes_struct!() =
+    eval(
+        Meta.parse(
+        "struct StokesArrays{A,B,C,T, nDim}
+            P::T
+            V::A
+            dV::A
+            ∇V::T
+            τ::B
+            R::C
+            
+            function StokesArrays(geometry::Geometry{2})
+                (; ni) = geometry
+                P = @zeros(ni...)
+                ∇V = @zeros(ni...)
+                Gdτ = @zeros(ni...)
+                dτ_Rho = @zeros(ni...)
+                V = Velocity(
+                    @zeros(ni[1]+1, ni[2]),
+                    @zeros(ni[1], ni[2]+1)
+                )
+                τ = SymmetricTensor(
+                    @zeros(ni...), # xx
+                    @zeros(ni[1]-1, ni[2]-1), # xy
+                    @zeros(ni...) # yy
+                )
+                dV = Velocity(
+                    @zeros(ni[1]-1, ni[2]-2),
+                    @zeros(ni[1]-2, ni[2]-1)
+                )
+
+                R = Residual(
+                    @zeros(ni[1]-1, ni[2]-2),
+                    @zeros(ni[1]-2, ni[2]-1)
+                )
+
+                new{typeof(V),typeof(τ),typeof(R),typeof(P), 2}(P, V, dV, ∇V, τ, R)
+            end
+        end"
+        )
+    )
+
+    
+make_PTstokes_struct!() =
+    eval(
+        Meta.parse(
+        "struct PTStokesCoeffs{T, nDim}
+            CFL::T
+            ϵ::T # PT tolerance
+            Re::T # Reynolds Number
+            r::T # 
+            Vpdτ::T
+            dτ_Rho::AbstractArray{T, nDim} 
+            Gdτ::AbstractArray{T, nDim}
+        
+            function PTStokesCoeffs(geometry::Geometry{nDim}; 
+                ϵ = 1e-8, Re = 5π, CFL = 0.9/√2, r=1e0) where {nDim}
+            
+                Vpdτ = min(geometry.di...)*CFL
+                Gdτ = @zeros(geometry.ni...)
+                dτ_Rho = @zeros(geometry.ni...)
+        
+                new{eltype(Gdτ), nDim}(CFL,ϵ,Re,r,Vpdτ,dτ_Rho,Gdτ)
+            end
+        
+        end"
+        )
+)
+
+PS_reset!() = ParallelStencil.@reset_parallel_stencil()
 
 end # module
---
+++ src/demo2.jl
@@ -0,0 +1,103 @@
+using Revise, OhMyREPL
+import Pkg; Pkg.activate(".")
+using ParallelStencil
+# using ParallelStencil.FiniteDifferences2D
+using JustRelax
+using GeoParams
+using Printf, LinearAlgebra, GLMakie
+
+# setup ParallelStencil.jl environment
+model = PS_Setup(:cpu, Float64, 2)
+environment!(model)
+
+# This main function for "Application" code is divided into 7 stages which
+# are intended to cover usage with the GPU4GEO project and potential users
+# of the software developed within it.
+
+# 1. Quantities needed to describe "where the problem lives", in terms of (parallel) topology
+# 2. Initialize tools which can represent this domain concretely in parallel (IGG here, could be PETSc/DM)
+# 3. Concrete representations of data and population of values
+#    - Includes information on embedding/coordinates
+# 4. Tools, dependent on the data representation, to actually solve a particular physical problem (here JustRelax.jl, but could be PETSc's SNES)
+#    - Note that here, the physical timestepping scheme is baked into this "physical problem"
+# 5. Analysis and output which depends on the details of the solver
+# 6. "Application" Analysis and output which does not depend on the details of the solver
+# 7. Finalization/Cleanup
+    
+# include benchmark related functions
+include("Stokes/Benchmarks/SolKz.jl")
+
+function solkz(; nx=256-1, ny=256-1, lx=1e0, ly=1e0)
+    ## Spatial domain: This object represents a rectangular domain decomposed into a Cartesian product of cells
+    # Here, we only explicitly store local sizes, but for some applications
+    # concerned with strong scaling, it might make more sense to define global sizes,
+    # independent of (MPI) parallelization
+    ni = (nx, ny) # number of nodes in x- and y-
+    li = (lx, ly)  # domain length in x- and y-
+    geometry = Geometry(ni, li) # structure containing topology information
+    g = 1
+
+    ## (Physical) Time domain and discretization
+    ttot = 1 # total simulation time
+    Δt = 1   # physical time step
+
+    ## Allocate arrays needed for every Stokes problem
+    # general stokes arrays
+    stokes = StokesArrays(geometry)
+    # general numerical coeffs for PT stokes
+    pt_stokes = PTStokesCoeffs(geometry)
+
+    ## Setup-specific parameters and fields
+    η = solKz_viscosity(geometry) # viscosity field
+    ρ = solKz_density(geometry)
+    fy = ρ*g
+
+    ## Boundary conditions
+    freeslip = (
+        freeslip_x = true,
+        freeslip_y = true
+    )
+
+    # Physical time loop
+    t = 0.0
+    while t < ttot
+        solve!(stokes, pt_stokes, geometry, freeslip, fy, η; iterMax = 10e3)
+        t += Δt
+    end
+
+    return geometry, stokes
+
+end
+
+# geometry, stokes = solkz(nx=31, ny=31)
+
+# # plot model output
+# f1 = plot_solkz(geometry, stokes)
+# # Compare pressure against analytical solution
+# f2 = plot_solkz_error(geometry, stokes)
+
+function run_test(; N = 9)
+    N = 9
+    L2_vx, L2_vy, L2_p = zeros(N), zeros(N), zeros(N)
+    for i in 1:N
+        nx = ny = 32*i-1
+        geometry, stokes = solkz(nx=nx, ny=ny)
+        L2_vx[i], L2_vy[i], L2_p[i] = Li_error(geometry, stokes, order=2)
+    end
+
+    nx = @. 32*(1:N)-1
+    h = @. (1/nx)
+
+    f = Figure( fontsize=28) 
+    ax = Axis(f[1,1], yscale = log10, xscale = log10,  yminorticksvisible = true, yminorticks = IntervalsBetween(8))
+    lines!(ax, h, (L2_vx), linewidth=3, label = "Vx")
+    lines!(ax, h, (L2_vy), linewidth=3, label = "Vy")
+    lines!(ax, h, (L2_p),  linewidth=3, label = "P")
+    axislegend(ax)
+    ax.xlabel = "h"
+    ax.ylabel = "L2 norm"
+    display(f)
+
+end
+
+run_test(N =9)

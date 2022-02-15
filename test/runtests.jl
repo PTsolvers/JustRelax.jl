67f3e4461278abc1ed3b4b27caee1d560f5c104f
Patrick Sanan <patrick.sanan@gmail.com>
2022-02-15 17:12:19 +0100

    Add test boilerplate, reinitialization, SolCx, and SolKz tests
    Add Julia test boilerplate and GitHub Actions workflow, with badge.
    
    Use the approach from MPI.jl and ParallelStencil.jl, wherein
    tests are run from independent, contained Julia scripts. Use the fact
    that Test.jl's macros throw exceptions on failed tests, and that
    Julia processes return  non-zero exit codes when exiting with an
    exception. This allows one to determine overall success
    by running all test_*.jl scripts.
    
    Add tests based on the SolCx and SolKz stokes benchmarks.
    
    Add a test for reinitializing (which produces a warning from
    ParallelStencil which should not be indefinitely ignored, as we
    are not running from the REPL).

---
+++ test/runtests.jl
@@ -0,0 +1,24 @@
+push!(LOAD_PATH, "..")
+
+function runtests()
+  exename = joinpath(Sys.BINDIR, Base.julia_exename())
+  testdir = pwd()
+  istest(f) = endswith(f, ".jl") && startswith(basename(f), "test_")
+  testfiles = sort(filter(istest, vcat([joinpath.(root, files) for (root, dirs, files) in walkdir(testdir)]...)))
+
+  nfail = 0
+  printstyled("Testing package JustRelax.jl\n"; bold=true, color=:white)
+
+  for f in testfiles
+    println("")
+    println("Running tests from $f")
+    try
+      run(`$exename -O3 --startup-file=no --check-bounds=no $(joinpath(testdir, f))`)
+    catch ex
+      nfail += 1
+    end
+  end
+  return nfail
+end
+
+exit(runtests())
---
+++ test/test_reinitialization.jl
@@ -0,0 +1,17 @@
+push!(LOAD_PATH, "..")
+
+using Test
+using ParallelStencil
+using JustRelax
+
+model = PS_Setup(:cpu, Float64, 2)
+environment!(model)
+
+ParallelStencil.@reset_parallel_stencil
+
+model = PS_Setup(:cpu, Float32, 2)
+environment!(model)
+
+@testset begin
+    @test true  # Success if no exception is thrown before this point
+end

2c2d5e7db781b8d560ea13ef1624eb2b1d628821
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-02-21 17:08:48 +0200

    Update BoundaryConditions.jl

--- src/boundaryconditions/BoundaryConditions.jl
+++ src/boundaryconditions/BoundaryConditions.jl
@@ -1,13 +1,3 @@
-function pureshear_bc!(stokes::StokesArrays, di::NTuple{2,T}, li::NTuple{2,T}, εbg) where T
-    # unpack
-    Vx, Vy = stokes.V.Vx, stokes.V.Vy
-    dx, dy = di 
-    lx, ly = li 
-    # Velocity boundary conditions
-    stokes.V.Vx .= PTArray( -εbg.*[((ix-1)*dx -0.5*lx) for ix=1:size(Vx,1), iy=1:size(Vx,2)] )
-    stokes.V.Vy .= PTArray(  εbg.*[((iy-1)*dy -0.5*ly) for ix=1:size(Vy,1), iy=1:size(Vy,2)] )
-end
-
 @parallel_indices (iy) function free_slip_x!(A::AbstractArray{eltype(PTArray), 2}) 
     A[1  , iy] = A[2    , iy]
     A[end, iy] = A[end-1, iy]

140a883a236f72d8de13622fa6bdf98a0b76bc1f
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-02-21 16:37:41 +0200

    Update RunStokesBench.jl
    Added solVi benchmark,  throw error,  and small changes

--- miniapps/benchmarks/stokes/RunStokesBench.jl
+++ miniapps/benchmarks/stokes/RunStokesBench.jl
@@ -1,6 +1,6 @@
 using ParallelStencil
 using JustRelax
-using Printf, LinearAlgebra, GLMakie
+using Printf, LinearAlgebra, CairoMakie
 
 # setup ParallelStencil.jl environment
 model = PS_Setup(:cpu, Float64, 2)
@@ -29,7 +29,6 @@ if benchmark == :solcx
     
     # viscosity contrast
     Δη = 1e6
-
     if runtype == :single
         # run model
         geometry, stokes, iters, ρ = solCx(Δη, nx=nx, ny=ny);
@@ -38,7 +37,7 @@ if benchmark == :solcx
         f = plot_solCx_error(geometry, stokes, Δη, cmap=:romaO)
         
     elseif runtype == :multiple
-        f = multiple_solCx(Δη = Δη, N = 10) # nx = ny = 2^(6:N)-1
+        f = multiple_solCx(Δη = Δη, nrange = 6:10) # nx = ny = 2^(nrange)-1
 
     end
 
@@ -52,16 +51,46 @@ elseif benchmark == :solkz
     # include plotting and error related functions
     include("solkz/SolKz.jl")
         
-    # viscosity contrast: Δη = 1e6
+    # viscosity contrast
+    Δη = 1e6
     if runtype == :single
         # run model
-        geometry, stokes, iters, ρ = solKz(nx=nx, ny=ny);
+        geometry, stokes, iters,  = solKz(Δη=Δη, nx=nx, ny=ny);
     
         # plot model output and error
         f = plot_solKz_error(geometry, stokes, cmap=:romaO)
     
     elseif runtype == :multiple
-        f = multiple_solKz(N = 10) # nx = ny = 2^(4:N)-1
+        f = multiple_solKz(Δη = Δη, nrange = 4:10) # nx = ny = 2^(nrange)-1
 
     end
+    
+elseif benchmark == :solvi
+    # Benchmark reference:
+    #   D. W. Schmid and Y. Y. Podladchikov. Analytical solutions for deformable elliptical inclusions in
+    #   general shear. Geophysical Journal International, 155(1):269–288, 2003.
+        
+    # include plotting and error related functions
+    include("solvi/SolVi.jl") # need to call this again if we switch from gpu <-/-> cpu
+    
+    # model specific parameters
+    Δη = 1e3 # viscosity ratio between matrix and inclusion
+    rc = 0.2 # radius of the inclusion
+    εbg = 1e0 # background strain rate
+    lx, ly = 2e0, 2e0 # domain siye in x and y directions
+    if runtype == :single
+        # run model
+        geometry, stokes, iters = solVi(Δη=Δη, nx=nx, ny=ny, lx=lx, ly=ly, rc = rc, εbg = εbg);
+            
+        # plot model output and error
+        f = plot_solVi_error(geometry, stokes, Δη, εbg, rc)
+        
+    elseif runtype == :multiple
+        f = multiple_solVi(; Δη=Δη, lx=lx, ly=ly, rc = rc, εbg = εbg, nrange=4:8) # nx = ny = 2^(nrange)-1
+    
+    end
+
+else
+    throw("Benchmark not available. List of benchmarks = :solcx, :solkz, :solvi")
+    
 end

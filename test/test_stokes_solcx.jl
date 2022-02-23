d7b8ee04355f05da5c2c446b8aafad018d7b6cc2
Patrick Sanan <patrick.sanan@gmail.com>
2022-02-23 14:58:42 +0100

    Tests: update test_stokes_solcx.jl reference

--- test/test_stokes_solcx.jl
+++ test/test_stokes_solcx.jl
@@ -16,9 +16,11 @@ function check_convergence_case1()
   Δη = 1e6
   geometry, stokes, iters, ρ = solCx(Δη, nx=nx, ny=ny)
   iters_expected = (iter = 4000,
-                    err_evo1 = [2.687660078359529, 0.24711842751138513, 0.011292850755750898, 0.0004920705891406875, 2.139574580520437e-5, 9.302233455551638e-7, 4.04431715618076e-8, 1.7583442731006563e-9],
+                    err_evo1 = [2.6882547170954827, 0.2473522008680851, 0.01130098855514106, 0.0004922739546726743, 2.139794848519281e-5, 9.300304245694199e-7, 4.0422237837643596e-8, 1.7568857404566392e-9],
                     err_evo2 = [500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0, 4000.0]
                    )
+  println(iters)
+  println(iters_expected)
   return iters.iter == iters_expected.iter && iters.err_evo1[end] ≈ iters_expected.err_evo1[end]
 end
 

3f699ad28e92ac51ea8171fcd38cbe3c06d0379b
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-03-04 18:05:41 +0100

    fix Burstedde benchmark and add plots

--- miniapps/benchmarks/stokes3D/burstedde/Burstedde.jl
+++ miniapps/benchmarks/stokes3D/burstedde/Burstedde.jl
@@ -1,66 +1,49 @@
 using ParallelStencil
+using ParallelStencil.FiniteDifferences3D
+using ImplicitGlobalGrid
 using JustRelax
 using Printf, LinearAlgebra, CairoMakie
-using ParallelStencil.FiniteDifferences3D
 
 # setup ParallelStencil.jl environment
 model = PS_Setup(:cpu, Float64, 3)
 environment!(model)
 
-# struct Viscosity2{T, M, F}
-#     val::T
-#     β::M
-#     f::F
-
-#     function Viscosity2(ni::NTuple{3, T}, β::M) where {T, M}
-#         val = @zeros(ni...)
-#         f = _viscosity(x::T, y::T, z::T, β::T) where T<:Real = exp(1 - β*( (1-x) + y*(1-y) + y*(1-z) ))
-        
-#         new{typeof(val), M, Function}(val, β, f)
-#     end
-# end
-
-# function viscosity!(η::Viscosity2, xi, β)
-
-#     x = [x for x in xi[1], _ in xi[2], _ in xi[3]]
-#     y = [y for _ in xi[1], y in xi[2], _ in xi[3]]
-#     z = [z for _ in xi[1], _ in xi[2], z in xi[3]]
-#     @parallel _viscosity!(η, x, y, z, β) 
-
-#     return η
-# end
-
-# @parallel function _viscosity!(η::Viscosity2, x, y, z) 
-#     # @all(η) = exp(1 - β*( @all(x)*(1-@all(x)) + @all(y)*(1-@all(y)) + @all(z)*(1-@all(z)) ))
-#     @all(η) = η.f( @all(x), @all(y), @all(z), η.β)
-#     return
-# end
-
-_viscosity(x::T, y::T, z::T, β::T) where T<:Real = exp(1 - β*( (1-x) + y*(1-y) + y*(1-z) ))
-
-@parallel function _viscosity!(η, x, y, z, β) 
-    # @all(η) = exp(1 - β*( @all(x)*(1-@all(x)) + @all(y)*(1-@all(y)) + @all(z)*(1-@all(z)) ))
-    @all(η) = _viscosity(@all(x), @all(y), @all(z), β)
+@parallel_indices (ix, iy, iz) function _viscosity!(η, x, y, z, β) 
+    η[ix, iy, iz] = exp(1 - β*( x[ix]*(1-x[ix]) + y[iy]*(1-y[iy]) + z[iz]*(1-z[iz]) ))
     return
 end
 
-function viscosity(xi, ni, β)
-
+function viscosity(xi, β)
+    ni = length.(xi)
     η = @allocate ni...
-    x = [x for x in xi[1], _ in xi[2], _ in xi[3]]
-    y = [y for _ in xi[1], y in xi[2], _ in xi[3]]
-    z = [z for _ in xi[1], _ in xi[2], z in xi[3]]
-    @parallel _viscosity!(η, x, y, z, β) 
+    @parallel (1:ni[1], 1:ni[2], 1:ni[3]) _viscosity!(η, xi[1], xi[2], xi[3], β) 
 
     return η
 end
 
 function body_forces(xi::NTuple{3, T}, η, β) where T
-    x, y, z = xi
-    nx, ny, nz = length.(xi)
-    fx = [body_forces_x(x[ix], y[iy], z[iz], η[ix, iy, iz], β) for ix in 1:nx, iy in 1:ny, iz in 1:nz]
-    fy = [body_forces_y(x[ix], y[iy], z[iz], η[ix, iy, iz], β) for ix in 1:nx, iy in 1:ny, iz in 1:nz]
-    fz = [body_forces_z(x[ix], y[iy], z[iz], η[ix, iy, iz], β) for ix in 1:nx, iy in 1:ny, iz in 1:nz]
+    xx, yy, zz = xi
+    x = [x for x in xx, y in yy, z in zz]
+    y = [y for x in xx, y in yy, z in zz]
+    z = [z for x in xx, y in yy, z in zz]
+
+    dηdx = @. -β*(1-2*x)*η;
+    dηdy = @. -β*(1-2*y)*η;
+    dηdz = @. -β*(1-2*z)*η;
+
+    fx = @. ((y*z+3*x^2*y^3*z)- η*(2+6*x*y)) -
+            dηdx*(2+4*x+2*y+6*x^2*y) -
+            dηdy*(x+x^3+y+2*x*y^2) -
+            dηdz*(-3*z-10*x*y*z);
+    fy = @. ((x*z+3*x^3*y^2*z)- η*(2+2*x^2+2*y^2))-
+            dηdx*(x+x^3+y+2*x*y^2)-
+            dηdy*(2+2*x+4*y+4*x^2*y)-
+            dηdz*(-3*z-5*x^2*z);
+    fz = @. ((x*y+x^3*y^3) - η*(-10*y*z))-
+            dηdx*(-3*z-10*x*y*z)-
+            dηdy*(-3*z-5*x^2*z)-
+            dηdz*(-4-6*x-6*y-10*x^2*y);
+            
     return fx, fy, fz
 end
 
@@ -69,16 +52,15 @@ static(x, y, z, η, β) = (1, -η, (1-2*x)*β*η, (1-2*y)*β*η, (1-2*z)*β*η)
 function body_forces_x(x, y, z, η, β)
     fx = (
         y*z + 3*x^2*y^3*z,
-        2+6x*y,
-        2+4x+2y+6x^2*y,
-        x+y+2x*y^2+x^3,
-        -3z-10x*y*z
+        2 + 6x*y,
+        2 + 4x + 2y + 6x^2*y,
+        x + y + 2x*y^2 + x^3,
+       -3z - 10x*y*z
     )
 
     st = static(x, y, z, η, β)
 
     return dot(st, fx)
-       
 end
 
 function body_forces_y(x, y, z, η, β)
@@ -87,32 +69,99 @@ function body_forces_y(x, y, z, η, β)
         2 + 2x^2 + 2y^2,
         x + y + 2x*y^2 + x^3,
         2 + 2x + 4y + 4x^2*y,
-        -3z - 5x^2*z
+       -3z - 5x^2*z
     )
 
     st = static(x, y, z, η, β)
 
     return dot(st, fy)
-       
 end
 
 function body_forces_z(x, y, z, η, β)
     fz = (
         x*y + x^3*y^3,
-        -10y*z,
-        -3z - 10x*y*z,
-        -3z - 5x^2*z,
-        -4 - 6x - 6y - 10x^2*y
+       -10y*z,
+       -3z - 10x*y*z,
+       -3z - 5x^2*z,
+       -4 - 6x - 6y - 10x^2*y
     )
 
     st = static(x, y, z, η, β)
 
     return dot(st, fz)
-       
+end
+
+function velocity!(stokes, xci, xvi)
+    xc, yc, zc = xci
+    xv, yv, zv = xvi
+    Vx, Vy, Vz = stokes.V.Vx, stokes.V.Vy, stokes.V.Vz
+
+    _velocity_x(x, y, z) =   x +  x^2 +  x*y +  x^3*y
+    _velocity_y(x, y, z) =   y +  x*y +  y^2 +  x^2*y^2
+    _velocity_z(x, y, z) = -2z - 3x*z - 3y*z - 5x^2*y*z
+
+    @parallel_indices (ix, iy, iz) function _velocity!(Vx, Vy, Vz, xc, yc, zc, xv, yv, zv)
+        # Vx
+        if (ix ≤ size(Vx, 1)) && (iy ≤ size(Vx, 2)) && (iz ≤ size(Vx, 3))
+            if (ix == size(Vx, 1)) || (iy == size(Vx, 2)) ||  (iz == size(Vx, 3)) || (ix == 1) || (iy == 1) ||  (iz ==1)
+                Vx[ix, iy, iz] = _velocity_x(xv[ix], yc[iy], zc[iz])
+            else
+                Vx[ix, iy, iz] = zero(eltype(Vx))
+            end
+        end
+        # Vy
+        if (ix ≤ size(Vy, 1)) && (iy ≤ size(Vy, 2)) && (iz ≤ size(Vy, 3))
+            if (ix == size(Vy, 1)) || (iy == size(Vy, 2)) ||  (iz == size(Vy, 3)) || (ix == 1) || (iy == 1) ||  (iz == 1)
+                Vy[ix, iy, iz] = _velocity_y(xc[ix], yv[iy], zc[iz])
+            else
+                Vy[ix, iy, iz] = zero(eltype(Vx))
+            end
+        end
+        # Vz
+        if (ix ≤ size(Vz, 1)) && (iy ≤ size(Vz, 2)) && (iz ≤ size(Vz, 3))
+            if (ix == size(Vz, 1)) || (iy == size(Vz, 2)) ||  (iz == size(Vz, 3)) || (ix == 1) || (iy == 1) ||  (iz ==1)
+                Vz[ix, iy, iz] = _velocity_z(xc[ix], yc[iy], zv[iz])
+            else
+                Vz[ix, iy, iz] = zero(eltype(Vx))
+            end
+        end
+        
+        return
+    end
+
+    @parallel _velocity!(Vx, Vy, Vz, xc, yc, zc, xv, yv, zv)
+
+end
+
+function analytical_velocity!(stokes, xci, xvi)
+    xc, yc, zc = xci
+    xv, yv, zv = xvi
+    Vx, Vy, Vz = stokes.V.Vx, stokes.V.Vy, stokes.V.Vz
+
+    _velocity_x(x, y, z) =   x + x^2 +x*y + x^3*y
+    _velocity_y(x, y, z) =   y + x*y + y^2 + x^2*y^2
+    _velocity_z(x, y, z) = -2z - 3x*z - 3y*z -5x^2*y*z
+
+    @parallel_indices (ix, iy, iz) function _velocity!(Vx, Vy, Vz, xc, yc, zc, xv, yv, zv)
+        if (ix ≤ size(Vx, 1)) && (iy ≤ size(Vx, 2)) && (iz ≤ size(Vx, 3))
+            Vx[ix, iy, iz] = _velocity_x(xv[ix], yc[iy], zc[iz])
+        end
+        if (ix ≤ size(Vy, 1)) && (iy ≤ size(Vy, 2)) && (iz ≤ size(Vy, 3))
+            Vy[ix, iy, iz] = _velocity_y(xc[ix], yv[iy], zc[iz])
+        end
+        if (ix ≤ size(Vz, 1)) && (iy ≤ size(Vz, 2)) && (iz ≤ size(Vz, 3))
+            Vz[ix, iy, iz] = _velocity_z(xc[ix], yc[iy], zv[iz])
+        end
+        
+        return
+    end
+
+    @parallel _velocity!(Vx, Vy, Vz, xc, yc, zc, xv, yv, zv)
+
 end
 
 function Burstedde()
-    nx=ny=nz=32
+    nx=ny=nz=64
     lx=ly=lz=1e0
     
     ## Spatial domain: This object represents a rectangular domain decomposed into a Cartesian product of cells
@@ -121,12 +170,17 @@ function Burstedde()
     # independent of (MPI) parallelization
     ni = (nx, ny, nz) # number of nodes in x- and y-
     li = (lx, ly, lz)  # domain length in x- and y-
-    di = @. li/ni # grid step in x- and -y
+    igg = IGG(
+        init_global_grid(nx, ny, nz)...
+    ) # init MPI
+    @static if USE_GPU select_device() end # select one GPU per MPI local rank (if >1 GPU per node)
+    li = (lx, ly, lz)  # domain length in x- and y-
+    di = @. li/(nx_g(), ny_g(), nz_g()) # grid step in x- and -y
     max_li = max(li...)
     xci, xvi = lazy_grid(di, li) # nodes at the center and vertices of the cells
 
     ## (Physical) Time domain and discretization
-    ttot = 1 # total simulation time
+    ttot = 1 # total siηlation time
     dt = 1   # physical time step
 
     ## Allocate arrays needed for every Stokes problem
@@ -136,30 +190,30 @@ function Burstedde()
     pt_stokes = PTStokesCoeffs(ni, di, Re = 6π, CFL = 0.9/√3)
 
     ## Setup-specific parameters and fields
-    β = 1.0
-    η = viscosity(xci, ni, β) # add reference 
+    β = 10.0
+    η = viscosity(xci, β) # add reference 
+    ρg = body_forces(xci, η, β) # => ρ*(gx, gy, gz)
     G = Inf
     
     ## Boundary conditions
     freeslip = (
-        freeslip_x = true,
-        freeslip_y = true,
-        freeslip_z = true
+        freeslip_x = false,
+        freeslip_y = false,
+        freeslip_z = false
     )
-
+    # impose analytical velociity at the boundaries of the domain
+    velocity!(stokes, xci, xvi)
+    
     # Physical time loop
     t = 0.0
-    ρg = body_forces(xci, η, β) # => ρ*(gx, gy, gz)
 
     local iters
     while t < ttot
-        iters = solve!(stokes, pt_stokes, di, li, max_li, freeslip, ρg, η, G, dt; iterMax = 10e3)
+        iters = solve!(stokes, pt_stokes, ni, di, li, max_li, freeslip, ρg, η, G, dt; iterMax = 10e3)
         t += dt
     end
 
+    finalize_global_grid()
+
     return (ni=ni, xci=xci, xvi=xvi, li=li, di=di), stokes, iters
 end
-
-# vtk_grid("fields", xci...) do vtk
-#     vtk["viscosity"] = η
-# end

cc52be1b2d39f1f4e03d197684a48ee2c5b2fcca
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-03-08 13:33:02 +0100

    @reexport support

--- src/JustRelax.jl
+++ src/JustRelax.jl
@@ -1,19 +1,16 @@
 module JustRelax
 
-using ParallelStencil
-using ImplicitGlobalGrid
+using Reexport
+@reexport using ParallelStencil
+@reexport using ImplicitGlobalGrid
 using LinearAlgebra
 using Printf
 using CUDA
-using Reexport
 
 include("topology/Topology.jl")
 include("MetaJustRelax.jl")
 include("stokes/MetaStokes.jl")
 
-@reexport import ParallelStencil
-
-# JustRelax.jl
 export PS_Setup, environment!, ps_reset!
 
 end # module

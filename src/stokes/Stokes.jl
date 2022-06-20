70e738c5123afd089359ed74930abb7d855cefdf
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-06-20 11:27:58 +0200

    harmonic averages

--- src/stokes/Stokes.jl
+++ src/stokes/Stokes.jl
@@ -56,18 +56,18 @@ end
     τxy::AbstractArray{eltype(PTArray),2},
     Vx::AbstractArray{eltype(PTArray),2},
     Vy::AbstractArray{eltype(PTArray),2},
-    Mus::AbstractArray{eltype(PTArray),2},
+    η::AbstractArray{eltype(PTArray),2},
     Gdτ::AbstractArray{eltype(PTArray),2},
     dx::Real,
     dy::Real,
 )
     @all(τxx) =
-        (@all(τxx) + 2.0 * @all(Gdτ) * @d_xa(Vx) / dx) / (@all(Gdτ) / @all(Mus) + 1.0)
+        (@all(τxx) + 2.0 * @all(Gdτ) * @d_xa(Vx) / dx) / (@all(Gdτ) / @all(η) + 1.0)
     @all(τyy) =
-        (@all(τyy) + 2.0 * @all(Gdτ) * @d_ya(Vy) / dy) / (@all(Gdτ) / @all(Mus) + 1.0)
+        (@all(τyy) + 2.0 * @all(Gdτ) * @d_ya(Vy) / dy) / (@all(Gdτ) / @all(η) + 1.0)
     @all(τxy) =
-        (@all(τxy) + 2.0 * @av(Gdτ) * (0.5 * (@d_yi(Vx) / dy + @d_xi(Vy) / dx))) /
-        (@av(Gdτ) / @av(Mus) + 1.0)
+        (@all(τxy) + 2.0 * @harm(Gdτ) * (0.5 * (@d_yi(Vx) / dy + @d_xi(Vy) / dx))) /
+        (@harm(Gdτ) / @harm(η) + 1.0)
     return nothing
 end
 
@@ -87,8 +87,8 @@ end
 )
     @all(Rx) = @d_xi(τxx) / dx + @d_ya(τxy) / dy - @d_xi(P) / dx
     @all(Ry) = @d_yi(τyy) / dy + @d_xa(τxy) / dx - @d_yi(P) / dy
-    @all(dVx) = @av_xi(dτ_Rho) * @all(Rx)
-    @all(dVy) = @av_yi(dτ_Rho) * @all(Ry)
+    @all(dVx) = @harm_xi(dτ_Rho) * @all(Rx)
+    @all(dVy) = @harm_yi(dτ_Rho) * @all(Ry)
     return nothing
 end
 
@@ -107,9 +107,9 @@ end
     dy::Real,
 )
     @all(Rx) = @d_xi(τxx) / dx + @d_ya(τxy) / dy - @d_xi(P) / dx
-    @all(Ry) = @d_yi(τyy) / dy + @d_xa(τxy) / dx - @d_yi(P) / dy - @av_yi(ρg)
-    @all(dVx) = @av_xi(dτ_Rho) * @all(Rx)
-    @all(dVy) = @av_yi(dτ_Rho) * @all(Ry)
+    @all(Ry) = @d_yi(τyy) / dy + @d_xa(τxy) / dx - @d_yi(P) / dy - @harm_yi(ρg)
+    @all(dVx) = @harm_xi(dτ_Rho) * @all(Rx)
+    @all(dVy) = @harm_yi(dτ_Rho) * @all(Ry)
     return nothing
 end
 
@@ -169,7 +169,6 @@ function solve!(
     Gdτ, dτ_Rho, ϵ, Re, r, Vpdτ = pt_stokes.Gdτ,
     pt_stokes.dτ_Rho, pt_stokes.ϵ, pt_stokes.Re, pt_stokes.r,
     pt_stokes.Vpdτ
-    freeslip_x, freeslip_y = freeslip
 
     # ~preconditioner
     ητ = deepcopy(η)
@@ -182,27 +181,21 @@ function solve!(
     cont = 0
     err_evo1 = Float64[]
     err_evo2 = Float64[]
-    err_rms = Float64[]
     norm_Rx = Float64[]
     norm_Ry = Float64[]
     norm_∇V = Float64[]
 
     # solver loop
+    wtime0 = 0.0
     while err > ϵ && iter <= iterMax
-        if (iter == 11)
-            global wtime0 = Base.time()
-        end
-        @parallel compute_P!(∇V, P, Vx, Vy, Gdτ, r, dx, dy)
-        @parallel compute_τ!(τxx, τyy, τxy, Vx, Vy, η, Gdτ, dx, dy)
-        @parallel compute_dV!(Rx, Ry, dVx, dVy, P, τxx, τyy, τxy, dτ_Rho, ρg, dx, dy)
-        @parallel compute_V!(Vx, Vy, dVx, dVy)
+        wtime0 += @elapsed begin
+            @parallel compute_P!(∇V, P, Vx, Vy, Gdτ, r, dx, dy)
+            @parallel compute_τ!(τxx, τyy, τxy, Vx, Vy, η, Gdτ, dx, dy)
+            @parallel compute_dV!(Rx, Ry, dVx, dVy, P, τxx, τyy, τxy, dτ_Rho, ρg, dx, dy)
+            @parallel compute_V!(Vx, Vy, dVx, dVy)
 
-        # free slip boundary conditions
-        if (freeslip_x)
-            @parallel (1:size(Vx, 1)) free_slip_y!(Vx)
-        end
-        if (freeslip_y)
-            @parallel (1:size(Vy, 2)) free_slip_x!(Vy)
+            # free slip boundary conditions
+            apply_free_slip!(freeslip, Vx, Vy)
         end
 
         iter += 1
@@ -216,7 +209,7 @@ function solve!(
             err = maximum([norm_Rx[cont], norm_Ry[cont], norm_∇V[cont]])
             push!(err_evo1, maximum([norm_Rx[cont], norm_Ry[cont], norm_∇V[cont]]))
             push!(err_evo2, iter)
-            if (verbose || err < ϵ || iter == iterMax)
+            if verbose && (err < ϵ) || (iter == iterMax)
                 @printf(
                     "Total steps = %d, err = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e, norm_∇V=%1.3e] \n",
                     iter,

b4f4ff89feba40317a88cb75be4dbab1a4c72d81
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-12-19 15:29:13 +0100

    fix few bugs

--- src/stokes/Elasticity.jl
+++ src/stokes/Elasticity.jl
@@ -93,8 +93,8 @@ end
 end
 
 @parallel function compute_strain_rate!(εxx, εyy, εxy, ∇V, Vx, Vy, _dx, _dy)
-    @all(εxx) =        @d_xi(Vx) * _dx - @all(∇V) / 3.0
-    @all(εyy) =        @d_yi(Vy) * _dy - @all(∇V) / 3.0
+    @all(εxx) = @d_xi(Vx) * _dx - @all(∇V) / 3.0
+    @all(εyy) = @d_yi(Vy) * _dy - @all(∇V) / 3.0
     @all(εxy) = 0.5 * (@d_ya(Vx) * _dy + @d_xa(Vy) * _dx)
     return nothing
 end
@@ -104,14 +104,14 @@ end
 ## Incompressible 
 @parallel function compute_P!(P, RP, ∇V, η, r, θ_dτ)
     @all(RP) = -@all(∇V)
-    @all(P)  =  @all(P) + @all(RP) * r / θ_dτ * @all(η)
+    @all(P) = @all(P) + @all(RP) * r / θ_dτ * @all(η)
     return nothing
 end
 
 ## Compressible 
 @parallel function compute_P!(P, P_old, RP, ∇V, η, K, dt, r, θ_dτ)
     @all(RP) = -@all(∇V) - (@all(P) - @all(P_old)) / (@all(K) * dt)
-    @all(P)  =  @all(P) +  @all(RP) / (1.0 / (r / θ_dτ * @all(η)) + 1.0 /(@all(K) * dt))
+    @all(P) = @all(P) + @all(RP) / (1.0 / (r / θ_dτ * @all(η)) + 1.0 / (@all(K) * dt))
     return nothing
 end
 
@@ -123,8 +123,14 @@ end
 # end
 
 @parallel function compute_V!(Vx, Vy, P, τxx, τyy, τxyv, ηdτ, ρgx, ρgy, ητ, _dx, _dy)
-    @inn(Vx) = @inn(Vx) + (-@d_xa(P) * _dx + @d_xa(τxx) * _dx + @d_yi(τxyv) * _dy - @all(ρgx)) * ηdτ / @harm_xa(ητ)
-    @inn(Vy) = @inn(Vy) + (-@d_ya(P) * _dy + @d_ya(τyy) * _dy + @d_xi(τxyv) * _dx - @all(ρgy)) * ηdτ / @harm_ya(ητ)
+    @inn(Vx) =
+        @inn(Vx) +
+        (-@d_xa(P) * _dx + @d_xa(τxx) * _dx + @d_yi(τxyv) * _dy - @av_xa(ρgx)) * ηdτ /
+        @harm_xa(ητ)
+    @inn(Vy) =
+        @inn(Vy) +
+        (-@d_ya(P) * _dy + @d_ya(τyy) * _dy + @d_xi(τxyv) * _dx - @av_ya(ρgy)) * ηdτ /
+        @harm_ya(ητ)
 
     return nothing
 end
@@ -132,12 +138,19 @@ end
 # Stress calculation
 
 # viscous
-@parallel function compute_τ!(
-    τxx, τyy, τxyv, τxx_o, τyy_o, τxyv_o, εxx, εyy, εxyv, η, θ_dτ
-)
-    @all(τxx)  = @all(τxx)  + (-( @all(τxx) -  @all(τxx_o)) -  @all(τxx) + 2 *  @all(η) *  @all(εxx)) * 1.0/(θ_dτ + 1.0)
-    @all(τyy)  = @all(τyy)  + (-( @all(τyy) -  @all(τyy_o)) -  @all(τyy) + 2 *  @all(η) *  @all(εyy)) * 1.0/(θ_dτ + 1.0)
-    @inn(τxyv) = @inn(τxyv) + (-(@inn(τxyv) - @inn(τxyv_o)) - @inn(τxyv) + 2 * @harm(η) * @inn(εxyv)) * 1.0/(θ_dτ + 1.0)
+@parallel function compute_τ!(τxx, τyy, τxyv, τxx_o, τyy_o, τxyv_o, εxx, εyy, εxyv, η, θ_dτ)
+    @all(τxx) =
+        @all(τxx) +
+        (-(@all(τxx) - @all(τxx_o)) - @all(τxx) + 2 * @all(η) * @all(εxx)) * 1.0 /
+        (θ_dτ + 1.0)
+    @all(τyy) =
+        @all(τyy) +
+        (-(@all(τyy) - @all(τyy_o)) - @all(τyy) + 2 * @all(η) * @all(εyy)) * 1.0 /
+        (θ_dτ + 1.0)
+    @inn(τxyv) =
+        @inn(τxyv) +
+        (-(@inn(τxyv) - @inn(τxyv_o)) - @inn(τxyv) + 2 * @harm(η) * @inn(εxyv)) * 1.0 /
+        (θ_dτ + 1.0)
 
     return nothing
 end
@@ -146,17 +159,47 @@ end
 @parallel function compute_τ!(
     τxx, τyy, τxyv, τxx_o, τyy_o, τxyv_o, εxx, εyy, εxyv, η, G, θ_dτ, dt
 )
-    @all(τxx)  = @all(τxx)  + (-( @all(τxx) -  @all(τxx_o)) -  @all(τxx) + 2 *  @all(η) *  @all(εxx)) * 1.0/(θ_dτ +  @all(η) /  (@all(G)*dt) + 1.0)
-    @all(τyy)  = @all(τyy)  + (-( @all(τyy) -  @all(τyy_o)) -  @all(τyy) + 2 *  @all(η) *  @all(εyy)) * 1.0/(θ_dτ +  @all(η) /  (@all(G)*dt) + 1.0)
-    @inn(τxyv) = @inn(τxyv) + (-(@inn(τxyv) - @inn(τxyv_o)) - @inn(τxyv) + 2 * @harm(η) * @inn(εxyv)) * 1.0/(θ_dτ + @harm(η) / (@harm(G)*dt) + 1.0)
+    @all(τxx) =
+        @all(τxx) +
+        (-(@all(τxx) - @all(τxx_o)) - @all(τxx) + 2 * @all(η) * @all(εxx)) * 1.0 /
+        (θ_dτ + @all(η) / (@all(G) * dt) + 1.0)
+    @all(τyy) =
+        @all(τyy) +
+        (-(@all(τyy) - @all(τyy_o)) - @all(τyy) + 2 * @all(η) * @all(εyy)) * 1.0 /
+        (θ_dτ + @all(η) / (@all(G) * dt) + 1.0)
+    @inn(τxyv) =
+        @inn(τxyv) +
+        (-(@inn(τxyv) - @inn(τxyv_o)) - @inn(τxyv) + 2 * @harm(η) * @inn(εxyv)) * 1.0 /
+        (θ_dτ + @harm(η) / (@harm(G) * dt) + 1.0)
 
     return nothing
 end
 
-@parallel function compute_Res!(Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy)
-    @inn_x(Rx) = -@d_xa(P) * _dx + @d_xa(τxx) * _dx + @d_yi(τxy) * _dy - @all(ρgx)
-    @inn_y(Ry) = -@d_ya(P) * _dy + @d_ya(τyy) * _dy + @d_xi(τxy) * _dx - @all(ρgy)
+# @parallel function compute_Res!(Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy)
+#     # @inn_x(Rx) = -@d_xa(P) * _dx + @d_xa(τxx) * _dx + @d_yi(τxy) * _dy - @all(ρgx)
+#     # @inn_y(Ry) = -@d_ya(P) * _dy + @d_ya(τyy) * _dy + @d_xi(τxy) * _dx - @all(ρgy)
+#     @all(Rx) = -@d_xa(P) * _dx + @d_xa(τxx) * _dx + @d_yi(τxy) * _dy - @av_xa(ρgx)
+#     @all(Ry) = -@d_ya(P) * _dy + @d_ya(τyy) * _dy + @d_xi(τxy) * _dx - @av_ya(ρgy)
 
+#     return nothing
+# end
+
+@parallel_indices (i, j) function compute_Res!(Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy)
+
+    # Again, indices i, j are captured by the closure
+    @inbounds @inline d_xa(A)  = (A[i+1, j  ] - A[i  , j  ]) * _dx 
+    @inbounds @inline d_ya(A)  = (A[i  , j+1] - A[i  , j  ]) * _dy
+    @inbounds @inline d_xi(A)  = (A[i+1, j+1] - A[i  , j+1]) * _dx
+    @inbounds @inline d_yi(A)  = (A[i+1, j+1] - A[i+1, j  ]) * _dy
+    @inbounds @inline av_xa(A)  = (A[i+1, j  ] + A[i  , j  ]) * 0.5
+    @inbounds @inline av_ya(A)  = (A[i  , j+1] + A[i  , j  ]) * 0.5
+
+    if i ≤ size(Rx, 1) && j ≤ size(Rx, 2)
+        @inbounds Rx[i, j] = d_xa(τxx) + d_yi(τxy) - d_xa(P) - av_xa(ρgx)
+    end
+    if i ≤ size(Ry, 1) && j ≤ size(Ry, 2)
+        @inbounds Ry[i, j] = d_ya(τyy) + d_xi(τxy) - d_ya(P) - av_ya(ρgy)
+    end
     return nothing
 end
 
@@ -167,7 +210,7 @@ end
 #     @inbounds @inline d_ya(A)  = (A[i+1, j+1] - A[i+1, j  ]) * _dy
 #     @inbounds @inline d_xi(A)  = (A[i+2, j+1] - A[i+1, j+1]) * _dx
 #     @inbounds @inline d_yi(A)  = (A[i+1, j+2] - A[i+1, j+1]) * _dy
-    
+
 #     if i ≤ size(Rx, 1) && j ≤ size(Rx, 2)
 #         @inbounds Rx[i, j] = d_xa(τxx) + d_yi(τxy) - d_xa(P) - ρgx[i  , j+1]
 #     end
@@ -203,7 +246,7 @@ function JustRelax.solve!(
     τxx_o, τyy_o, τxy_o = τ_o
     P, ∇V = stokes.P, stokes.∇V
     Rx, Ry, RP = stokes.R.Rx, stokes.R.Ry, stokes.R.RP
-    ϵ, r, θ_dτ, ηdτ = pt_stokes.ϵ,  pt_stokes.r, pt_stokes.θ_dτ, pt_stokes.ηdτ
+    ϵ, r, θ_dτ, ηdτ = pt_stokes.ϵ, pt_stokes.r, pt_stokes.θ_dτ, pt_stokes.ηdτ
     nx, ny = size(P)
 
     ρgx, ρgy = ρg
@@ -226,21 +269,23 @@ function JustRelax.solve!(
     wtime0 = 0.0
     while iter < 2 || (err > ϵ && iter ≤ iterMax)
         wtime0 += @elapsed begin
-            @parallel compute_∇V!(∇V, Vx, Vy, _dx, _dy)
-            @parallel compute_strain_rate!(εxx, εyy, εxy, ∇V, Vx, Vy, _dx, _dy)
-            @parallel compute_P!(P, P_old, RP, ∇V, η, K, dt, r, θ_dτ)
-            @parallel compute_τ!(
-                τxx, τyy, τxy, τxx_o, τyy_o, τxy_o, εxx, εyy, εxy, η, θ_dτ
-            )
-            @parallel compute_V!(Vx, Vy, P, τxx, τyy, τxy, ηdτ, ρgx, ρgy, ητ, _dx, _dy)
 
             # free slip boundary conditions
             apply_free_slip!(freeslip, Vx, Vy)
+            
+            @parallel compute_∇V!(∇V, Vx, Vy, _dx, _dy)
+            @parallel compute_strain_rate!(εxx, εyy, εxy, ∇V, Vx, Vy, _dx, _dy)
+            @parallel compute_P!(P, P_old, RP, ∇V, η, K, dt, r, θ_dτ)
+            @parallel compute_τ!(τxx, τyy, τxy, τxx_o, τyy_o, τxy_o, εxx, εyy, εxy, η, θ_dτ)
+            @parallel compute_V!(Vx, Vy, P, τxx, τyy, τxy, ηdτ, ρgx, ρgy, ητ, _dx, _dy)
+
         end
 
         iter += 1
         if iter % nout == 0 && iter > 1
-            @parallel (1:nx-1, 1:ny-1) compute_Res!(Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy)
+            @parallel (1:(nx - 1), 1:(ny - 1)) compute_Res!(
+                Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy
+            )
 
             # push!(norm_Rx, maximum(abs.(Rx)))
             # push!(norm_Ry, maximum(abs.(Ry)))
@@ -254,7 +299,7 @@ function JustRelax.solve!(
             err = max(norm_Rx[end], norm_Ry[end], norm_∇V[end])
             push!(err_evo1, err)
             push!(err_evo2, iter)
-            if( verbose && err > ϵ) || (iter == iterMax)
+            if (verbose && err > ϵ) || (iter == iterMax)
                 @printf(
                     "Total steps = %d, err = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e, norm_∇V=%1.3e] \n",
                     iter,
@@ -304,7 +349,7 @@ function JustRelax.solve!(
     τxx_o, τyy_o, τxy_o = τ_o
     P, ∇V = stokes.P, stokes.∇V
     Rx, Ry, RP = stokes.R.Rx, stokes.R.Ry, stokes.R.RP
-    ϵ, r, θ_dτ, ηdτ = pt_stokes.ϵ,  pt_stokes.r, pt_stokes.θ_dτ, pt_stokes.ηdτ
+    ϵ, r, θ_dτ, ηdτ = pt_stokes.ϵ, pt_stokes.r, pt_stokes.θ_dτ, pt_stokes.ηdτ
     nx, ny = size(P)
 
     ρgx, ρgy = ρg
@@ -327,7 +372,6 @@ function JustRelax.solve!(
     wtime0 = 0.0
     while iter < 2 || (err > ϵ && iter ≤ iterMax)
         wtime0 += @elapsed begin
-
             @parallel compute_∇V!(∇V, Vx, Vy, _dx, _dy)
             @parallel compute_strain_rate!(εxx, εyy, εxy, ∇V, Vx, Vy, _dx, _dy)
             @parallel compute_P!(P, P_old, RP, ∇V, η, K, dt, r, θ_dτ)
@@ -342,7 +386,9 @@ function JustRelax.solve!(
 
         iter += 1
         if iter % nout == 0 && iter > 1
-            @parallel (1:nx-1, 1:ny-1) compute_Res!(Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy)
+            @parallel (1:(nx - 1), 1:(ny - 1)) compute_Res!(
+                Rx, Ry, P, τxx, τyy, τxy, ρgx, ρgy, _dx, _dy
+            )
 
             push!(norm_Rx, maximum(abs.(Rx)))
             push!(norm_Ry, maximum(abs.(Ry)))
@@ -350,7 +396,7 @@ function JustRelax.solve!(
             err = max(norm_Rx[end], norm_Ry[end], norm_∇V[end])
             push!(err_evo1, err)
             push!(err_evo2, iter)
-            if( verbose && err > ϵ) || (iter == iterMax)
+            if (verbose && err > ϵ) || (iter == iterMax)
                 @printf(
                     "Total steps = %d, err = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e, norm_∇V=%1.3e] \n",
                     iter,

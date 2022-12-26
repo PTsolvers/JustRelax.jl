8bd51157731882559a8a8af908147776f6297b56
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-12-26 11:45:22 +0100

    adapt to damping v3.0

--- miniapps/benchmarks/stokes3D/solvi/SolVi3D.jl
+++ miniapps/benchmarks/stokes3D/solvi/SolVi3D.jl
@@ -53,12 +53,8 @@ function solVi3D(;
     # independent of (MPI) parallelization
     ni = (nx, ny, nz) # number of nodes in x- and y-
     igg = IGG(init_global_grid(nx, ny, nz; init_MPI=init_MPI)...) # init MPI
-    @static if USE_GPU # select one GPU per MPI local rank (if >1 GPU per node)
-        select_device()
-    end
     li = (lx, ly, lz)  # domain length in x- and y-
     di = @. li / (nx_g(), ny_g(), nz_g()) # grid step in x- and -y
-    max_li = max(li...)
     xci, xvi = lazy_grid(di, li) # nodes at the center and vertices of the cells
 
     ## (Physical) Time domain and discretization
@@ -78,6 +74,8 @@ function solVi3D(;
     G = 1.0 # elastic shear modulus
     dt = η0 / (G * ξ)
     η = viscosity(ni, di, li, rc, η0, ηi)
+    Gc = @fill(G, ni...) 
+    K = @fill(Inf, ni...) 
 
     ## Boundary conditions
     pureshear_bc!(stokes, di, li, εbg)
@@ -93,19 +91,17 @@ function solVi3D(;
         iters = solve!(
             stokes,
             pt_stokes,
-            ni,
             di,
             li,
-            max_li,
             freeslip,
             ρg,
             η,
-            G,
+            K,
+            Gc,
             dt,
             igg;
             iterMax=5000,
-            nout=2000,
-            b_width=(1, 1, 1),
+            nout=100,
         )
         t += dt
     end

96b216d2582d571c3e9d6278d225ab916aaca8e5
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-01-30 17:10:18 +0100

    fix test

--- test/test_stokes_solvi3D.jl
+++ test/test_stokes_solvi3D.jl
@@ -31,13 +31,15 @@ function check_convergence_case1()
         lz=lz,
         rc=rc,
         εbg=εbg,
-        init_MPI=true,
-        finalize_MPI=true,
+        init_MPI=false,
+        finalize_MPI=false,
     )
 
     iters_expected = (iter=500, err_evo1=[ 7.493148324044796e-10])
-    return iters.iter == iters_expected.iter &&
+    passed = iters.iter == iters_expected.iter &&
        iters.err_evo1[end] ≈ iters_expected.err_evo1[end]
+
+    return passed
 end
 
 @testset begin

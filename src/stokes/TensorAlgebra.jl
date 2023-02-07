430201795401e0044505a82b3426742eebb6e577
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-02-07 15:43:14 +0100

    finished (?) 3D rotation kernels

--- src/stokes/TensorAlgebra.jl
+++ src/stokes/TensorAlgebra.jl
@@ -1,10 +1,10 @@
 @parallel_indices (i, j) function rotate_stress!(V, τ::NTuple{3, T}, _di, dt) where T
-    @inline rotate_stress!(V, τ, (i, j) , _di, dt)
+    @inbounds rotate_stress!(V, τ, (i, j), _di, dt)
     return nothing
 end
 
 @parallel_indices (i, j, k) function rotate_stress!(V, τ::NTuple{6, T}, _di, dt) where T
-    @inline rotate_stress!(V, τ, (i, j, k) , _di, dt)
+    @inbounds rotate_stress!(V, τ, (i, j, k) , _di, dt)
     return nothing
 end
 
@@ -14,33 +14,32 @@ end
 τij_o += v_k * ∂τij_o/∂x_k - ω_ij * ∂τkj_o + ∂τkj_o * ω_ij
 
 """
-function rotate_stress!(V, τ::NTuple{N, T}, idx, _di, dt) where {N, T}
-
+Base.@propagate_inbounds function rotate_stress!(V, τ::NTuple{N, T}, idx, _di, dt) where {N, T}
     ## 1) Advect stress
-    Vᵢⱼ = @inbounds velocity2center(V..., idx...) # averages @ cell center
-    τij_adv = @inbounds advect_stress(τ..., Vᵢⱼ..., idx..., _di...)
+    Vᵢⱼ = velocity2center(V..., idx...) # averages @ cell center
+    τij_adv = advect_stress(τ..., Vᵢⱼ..., idx..., _di...)
 
     ## 2) Rotate stress
     # average ∂Vx/∂y @ cell center
-    ∂V∂x = @inbounds cross_derivatives(V..., _di..., idx...)
+    ∂V∂x = cross_derivatives(V..., _di..., idx...)
     # compute xy component of the vorticity tensor; normal components = 0.0
-    ω = @inbounds compute_vorticity(∂V∂x)
+    ω = compute_vorticity(∂V∂x)
     # stress tensor in Voigt notation
     τ_voigt = ntuple(Val(N)) do k
         Base.@_inline_meta
-        @inbounds τ[k][idx...]
+        τ[k][idx...]
     end
-    # rotate stress tensor
+    # actually rotate stress tensor
     τr_voigt = GeoParams.rotate_elastic_stress2D(ω, τ_voigt, dt)
     
     ## 3) Update stress
     for k in 1:N    
-        @inbounds τ[k][idx...] += muladd(τij_adv[k], dt, τr_voigt[k])
-        # @inbounds τ[k][idx...] += dt * (τr_voigt[k] + τij_adv[k])
+        τ[k][idx...] += muladd(τij_adv[k], dt, τr_voigt[k])
     end
     return 
 end
 
+# 2D
 Base.@propagate_inbounds function advect_stress(τxx, τyy, τxy, Vx, Vy, i, j, _dx, _dy)
     τ = τxx, τyy, τxy
     τ_adv = ntuple(Val(3)) do k
@@ -51,33 +50,76 @@ Base.@propagate_inbounds function advect_stress(τxx, τyy, τxy, Vx, Vy, i, j, 
     return τ_adv
 end
 
-function advection_term(Vx, Vy, dx_right, dx_left, dy_up, dy_down, _dx, _dy)
-    return (Vx > 0 ? dx_right : dx_left) * Vx * _dx +
-           (Vy > 0 ? dy_up : dy_down) * Vy * _dy
+# 3D
+Base.@propagate_inbounds function advect_stress(τxx, τyy, τzz, τyz, τxz, τxy, Vx, Vy, Vz, i, j, k, _dx, _dy, _dz)
+    τ = τxx, τyy, τzz, τyz, τxz, τxy
+    τ_adv = ntuple(Val(6)) do k
+        Base.@_inline_meta
+        dx_right, dx_left, dy_up, dy_down = upwind_derivatives(τ[k], i, j, k)
+        advection_term(Vx, Vy, Vz, dx_right, dx_left, dy_back, dy_front, dz_up, dz_down, _dx, _dy, _dz)
+    end
+    return τ_adv
 end
 
+# 2D
 Base.@propagate_inbounds function upwind_derivatives(A, i, j)
-    nx, ny =  size(A)
-
-    # dx derivatives
-    x_left   = i - 1 > 1 ? A[i-1, j] : 0.0
-    x_center = A[i, j]
-    x_right  = i + 1 < nx ? A[i+1, j] : 0.0
-    dx_right = x_right  - x_center
-    dx_left  = x_center - x_left
     
+    nx, ny   = size(A)
+    center   = A[i, j]
+    # dx derivatives
+    x_left   = i - 1 > 1  ? A[i-1, j] : 0.0
+    x_right  = i + 1 < nx ? A[i+1, j] : 0.0
+    dx_right = x_right - center
+    dx_left  = center - x_left
     # dy derivatives
-    y_down   = j - 1 > 1 ? A[i, j-1] : 0.0
-    y_center = A[i, j]
+    y_down   = j - 1 > 1  ? A[i, j-1] : 0.0
     y_up     = j + 1 < ny ? A[i, j+1] : 0.0
-    dy_up    = y_up - y_center
-    dy_down  = y_center - y_down
+    dy_up    = y_up - center
+    dy_down  = center - y_down
 
     return dx_right, dx_left, dy_up, dy_down
 end
 
+# 3D
+Base.@propagate_inbounds function upwind_derivatives(A, i, j, k)
+    
+    nx, ny, nz = size(A)
+    center     = A[i, j, k]
+    x_left = x_right = y_front = y_back = z_down = z_up = 0.0
+    # dx derivatives
+    i - 1 > 1  && (x_left = A[i-1, j, k])
+    i + 1 < nx && (x_right = A[i+1, j, k])
+    dx_right   = x_right - center
+    dx_left    = center - x_left
+    # dy derivatives
+    j - 1 > 1  && (y_front = A[i, j-1, k])
+    j + 1 < ny && (y_back = A[i, j+1, k])
+    dy_back    = y_back - center
+    dy_front   = center - y_front
+    # dz derivatives
+    k - 1 > 1  && (z_down = A[i, j, k-1])
+    k + 1 < nz && (z_up = A[i, j, k+1])
+    dz_up      = z_up - center
+    dz_down    = center - z_down
+
+    return dx_right, dx_left, dy_back, dy_front, dz_up, dz_down
+end
+
+# 2D
+@inline function advection_term(Vx, Vy, dx_right, dx_left, dy_up, dy_down, _dx, _dy)
+    return (Vx > 0 ? dx_right : dx_left) * Vx * _dx +
+           (Vy > 0 ? dy_up    : dy_down) * Vy * _dy
+end
+
+# 3D
+@inline function advection_term(Vx, Vy, Vz, dx_right, dx_left, dy_back, dy_front, dz_up, dz_down, _dx, _dy, _dz)
+    return (Vx > 0 ? dx_right : dx_left ) * Vx * _dx +
+           (Vy > 0 ? dy_back  : dy_front) * Vy * _dy +
+           (Vz > 0 ? dz_up    : dz_down ) * Vz * _dz
+end
+
 # averages @ cell center 2D
-Base.@propagate_inbounds @inline function velocity2center(Vx, Vy, i, j)
+Base.@propagate_inbounds function velocity2center(Vx, Vy, i, j)
     i1, j1 = @add 1 i j
     Vxᵢⱼ = 0.5 * (Vx[i , j1] + Vx[i1, j1])
     Vyᵢⱼ = 0.5 * (Vy[i1, j ] + Vy[i1, j1])
@@ -85,7 +127,7 @@ Base.@propagate_inbounds @inline function velocity2center(Vx, Vy, i, j)
 end
 
 # averages @ cell center 3D
-Base.@propagate_inbounds @inline function velocity2center(Vx, Vy, Vz, i, j, k)
+Base.@propagate_inbounds function velocity2center(Vx, Vy, Vz, i, j, k)
     i1, j1, k1 = @add 1 i j k
     Vxᵢⱼ = 0.5 * (Vx[i , j1, k1] + Vx[i1, j1, k1])
     Vyᵢⱼ = 0.5 * (Vy[i1, j , k1] + Vy[i1, j1, k1])
@@ -94,7 +136,7 @@ Base.@propagate_inbounds @inline function velocity2center(Vx, Vy, Vz, i, j, k)
 end
 
 # 2D
-Base.@propagate_inbounds @inline function cross_derivatives(Vx, Vy, _dx, _dy, i, j)
+Base.@propagate_inbounds function cross_derivatives(Vx, Vy, _dx, _dy, i, j)
     i1, j1 = @add 1 i j
     i2, j2 = @add 2 i j
     # average @ cell center
@@ -113,46 +155,45 @@ Base.@propagate_inbounds @inline function cross_derivatives(Vx, Vy, _dx, _dy, i,
     return ∂Vx∂y, ∂Vy∂x
 end
 
-# TOFINISH 3D
-Base.@propagate_inbounds @inline function cross_derivatives(Vx, Vy, Vz, _dx, _dy, _dz, i, j, k)
+Base.@propagate_inbounds function cross_derivatives(Vx, Vy, Vz, _dx, _dy, _dz, i, j, k)
     i1, j1, k2 = @add 1 i j k
     i2, j2, k2 = @add 2 i j k
     # cross derivatives @ cell centers
     ∂Vx∂y = 0.25 * _dy * (
-        Vx[i , j1, k] - Vx[i , j , k] + 
-        Vx[i , j2, k] - Vx[i , j1, k] + 
-        Vx[i1, j1, k] - Vx[i1, j , k] + 
-        Vx[i1, j2, k] - Vx[i1, j1, k]
+        Vx[i , j1, k1] - Vx[i , j , k1] + 
+        Vx[i , j2, k1] - Vx[i , j1, k1] + 
+        Vx[i1, j1, k1] - Vx[i1, j , k1] + 
+        Vx[i1, j2, k1] - Vx[i1, j1, k1]
     )
     ∂Vx∂z = 0.25 * _dz * (
-        Vx[i , j1, k] - Vx[i , j , k] + 
-        Vx[i , j2, k] - Vx[i , j1, k] + 
-        Vx[i1, j1, k] - Vx[i1, j , k] + 
-        Vx[i1, j2, k] - Vx[i1, j1, k]
+        Vx[i , j1, k1] - Vx[i , j , k ] + 
+        Vx[i , j2, k2] - Vx[i , j1, k1] + 
+        Vx[i1, j1, k1] - Vx[i1, j , k ] + 
+        Vx[i1, j2, k2] - Vx[i1, j1, k1]
     )
     ∂Vy∂x = 0.25 * _dx * (
-        Vy[i1, j , k] - Vy[i , j , k] + 
-        Vy[i2, j , k] - Vy[i1, j , k] + 
-        Vy[i1, j1, k] - Vy[i , j1, k] + 
-        Vy[i2, j1, k] - Vy[i1, j1, k]
+        Vy[i1, j , ki] - Vy[i , j , ki] + 
+        Vy[i2, j , ki] - Vy[i1, j , ki] + 
+        Vy[i1, j1, ki] - Vy[i , j1, ki] + 
+        Vy[i2, j1, ki] - Vy[i1, j1, ki]
     )
     ∂Vy∂z = 0.25 * _dz * (
-        Vy[i1, j , k] - Vy[i , j , k] + 
-        Vy[i2, j , k] - Vy[i1, j , k] + 
-        Vy[i1, j1, k] - Vy[i , j1, k] + 
-        Vy[i2, j1, k] - Vy[i1, j1, k]
+        Vy[i1, j , k1] - Vy[i , j , k ] + 
+        Vy[i2, j , k2] - Vy[i1, j , k1] + 
+        Vy[i1, j1, k1] - Vy[i , j1, k ] + 
+        Vy[i2, j1, k2] - Vy[i1, j1, k1]
     )
     ∂Vz∂x = 0.25 * _dx * (
-        Vz[i1, j , k] - Vz[i , j , k] + 
-        Vz[i2, j , k] - Vz[i1, j , k] + 
-        Vz[i1, j1, k] - Vz[i , j1, k] + 
-        Vz[i2, j1, k] - Vz[i1, j1, k]
+        Vz[i1, j , k ] - Vz[i , j , k ] + 
+        Vz[i2, j , k ] - Vz[i1, j , k ] + 
+        Vz[i1, j1, k1] - Vz[i , j1, 1k] + 
+        Vz[i2, j1, k1] - Vz[i1, j1, 1k]
     )
-    ∂Vz∂y = 0.25 * _dx * (
-        Vz[i1, j , k] - Vz[i , j , k] + 
-        Vz[i2, j , k] - Vz[i1, j , k] + 
-        Vz[i1, j1, k] - Vz[i , j1, k] + 
-        Vz[i2, j1, k] - Vz[i1, j1, k]
+    ∂Vz∂y = 0.25 * _dy * (
+        Vz[i1, j , k ] - Vz[i , j , k ] + 
+        Vz[i2, j , k ] - Vz[i1, j , k ] + 
+        Vz[i1, j1, k1] - Vz[i , j1, k1] + 
+        Vz[i2, j1, k1] - Vz[i1, j1, k1]
     )        
     return ∂Vx∂y, ∂Vx∂z, ∂Vy∂x, ∂Vy∂z, ∂Vz∂x, ∂Vz∂y
 end
@@ -160,16 +201,16 @@ end
 Base.@propagate_inbounds @inline compute_vorticity(∂V∂x::NTuple{2, T}) where T = ∂V∂x[1] - ∂V∂x[2] # 2D
 Base.@propagate_inbounds @inline compute_vorticity(∂V∂x::NTuple{3, T}) where T = ∂V∂x[3] - ∂V∂x[2], ∂V∂x[1] - ∂V∂x[3], ∂V∂x[2] - ∂V∂x[1] # 3D
 
-n = 128
-Vx = rand(n+1,n+2)
-Vy = rand(n+2,n+1)
-txx= rand(n,n)
-tyy= rand(n,n)
-txy= rand(n,n)
-V = Vx, Vy
-τ = txx, tyy, txy
-_di = rand(),rand()
-dt = 1.0
+# n = 128
+# Vx = rand(n+1,n+2)
+# Vy = rand(n+2,n+1)
+# txx= rand(n,n)
+# tyy= rand(n,n)
+# txy= rand(n,n)
+# V = Vx, Vy
+# τ = txx, tyy, txy
+# _di = rand(),rand()
+# dt = 1.0
 
 
 # @btime @parallel $(1:n, 1:n) rotate_stress!($V, $τ, $_di, $dt)

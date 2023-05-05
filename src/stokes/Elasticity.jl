fa1dd46eed734d84384767a248fa0162f9f2b21c
Pascal Aellig <pascal.aellig@hotmail.com>
2023-05-05 16:23:57 +0200

    indexing error

--- src/stokes/Elasticity.jl
+++ src/stokes/Elasticity.jl
@@ -314,7 +314,7 @@ end
 
     ρg[i, j] =
         compute_density_ratio((1-ϕ[i, j],ϕ[i, j],0.0), rheology, (; T = av(args.T), P=args.P[i, j])) *
-        compute_gravity(rheology[1],phase_c)
+        compute_gravity(rheology[1],phase_c[i, j])
     return nothing
 end
 

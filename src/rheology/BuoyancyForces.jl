37e6b278912c7381886160cad6c3801a92a4c526
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-15 23:02:09 +0200

    improve readibility

--- src/rheology/BuoyancyForces.jl
+++ src/rheology/BuoyancyForces.jl
@@ -7,11 +7,13 @@ Calculate the buoyance forces `ρg` for the given GeoParams.jl `rheology` object
 
     i1, j1 = i + 1, j + 1
     i2 = i + 2
-    @inline av(T) = 0.25 * (T[i1, j] + T[i2, j] + T[i1, j1] + T[i2, j1]) - 273.0
+    @inline av_T() = 0.25 * (T[i1, j] + T[i2, j] + T[i1, j1] + T[i2, j1]) - 273.0
     
     # index arguments for the current cell cell center
-    args_ij = (; T = av(args.T), P=args.P[i, j])
-
+    T = av_T()
+    P = args.P[i, j]
+    args_ij =(; T, P)
+    
     # compute ρg = density * gravity
     @inbounds ρg[i, j] = -compute_ρg(rheology, args_ij)
 
@@ -23,7 +25,9 @@ end
     av_T() = _av(args.T, i, j, k) - 273.0
 
     # index arguments for the current cell cell center
-    args_ijk =(; T = av_T(), P=args.P[i, j, k])
+    T = av_T()
+    P = args.P[i, j, k]
+    args_ijk =(; T, P)
 
     ρg[i, j, k] = compute_ρg(rheology, args_ijk)
     
@@ -40,10 +44,12 @@ The `phase_ratios` are used to compute the density of the composite rheology.
 
     i1, j1 = i + 1, j + 1
     i2 = i + 2
-    @inline av(T) = 0.25 * (T[i1,j] + T[i2,j] + T[i1,j1] + T[i2,j1]) - 273.0
-
+    @inline av_T() = 0.25 * (T[i1, j] + T[i2, j] + T[i1, j1] + T[i2, j1]) - 273.0
+    
     # index arguments for the current cell cell center
-    args_ij = (; T = av(args.T), P=args.P[i, j])
+    T = av_T()
+    P = args.P[i, j]
+    args_ij =(; T, P)
 
     # compute ρg = density * gravity
     @inbounds ρg[i, j] = -compute_ρg(rheology, args_ij, phase_ratios[i, j])
@@ -56,7 +62,9 @@ end
     av_T() = _av(args.T, i, j, k) - 273.0
 
     # index arguments for the current cell cell center
-    args_ijk =(; T = av_T(), P=args.P[i, j, k])
+    T = av_T()
+    P = args.P[i, j, k]
+    args_ijk =(; T, P)
 
     ρg[i, j, k] = compute_ρg(rheology, args_ijk, phase_ratios[i, j, k])
     return nothing

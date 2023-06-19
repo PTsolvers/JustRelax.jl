47f747b0c27c5b0328fd375deb7fe5654f15573d
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-19 17:12:21 +0200

    add method for single phase material

--- src/rheology/Viscosity.jl
+++ src/rheology/Viscosity.jl
@@ -1,3 +1,30 @@
+
+@parallel_indices (i, j) function compute_viscosity!(η, ν, εxx, εyy, εxyv, args, rheology)
+
+    # convinience closure
+    @inline gather(A) = _gather(A, i, j)
+   
+    # we nee strain rate not to be zero, otherwise we get NaNs
+    εII_0 = (εxx[i, j] == 0 && εyy[i, j] == 0) ? 1e-15 : 0.0
+
+    # argument fields at local index
+    v = getindex.(values(args), i, j)
+    k = keys(args)
+    args_ij  = (; zip(k, v)..., dt = args.dt, τII_old=0.0)
+
+    # cache strain rate and stress 
+    εij_p    = εII_0 + εxx[i, j], -εII_0 + εyy[i, j], gather(εxyv)
+    τij_p_o  = 0.0, 0.0, (0.0, 0.0, 0.0, 0.0)
+    phases   = 1, 1, (1,1,1,1) # for now hard-coded for a single phase
+
+    # update stress and effective viscosity
+    _, _, ηi = compute_τij(rheology, εij_p, args_ij, τij_p_o, phases)
+    ηi       = exp((1-ν)*log(η[i, j]) + ν*log(ηi))
+    η[i, j]  = clamp(ηi, 1e16, 1e24)
+    
+    return nothing
+end
+
 @parallel_indices (i, j) function compute_viscosity!(η, ν, ratios_center, εxx, εyy, εxyv, args, MatParam)
 
     # convinience closure
@@ -13,8 +40,9 @@
         # # update stress and effective viscosity
         _, _, ηi = compute_τij_ratio(MatParam, ratio_ij, εij_p, args_ij, τij_p_o)
         ηi       = exp((1-ν)*log(η[i, j]) + ν*log(ηi))
-        η[i, j]  = clamp(2*ηi, 1e16, 1e24)
+        η[i, j]  = clamp(ηi, 1e16, 1e24)
     end
     
     return nothing
 end
+

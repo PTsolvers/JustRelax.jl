0395e685c1d8fb644a70a7add915f92ac364e40c
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-06-22 15:56:10 +0200

    export 3D minikernels

--- src/JustRelax.jl
+++ src/JustRelax.jl
@@ -16,7 +16,9 @@ include("topology/Topology.jl")
 export IGG, lazy_grid, Geometry
 
 include("MiniKernels.jl")
-export _d_xa, _d_ya, _d_xi, _d_yi, _av, _av_xa, _av_ya, _gather, _gather_yz, _gather_xz, _gather_xy
+export _d_xa, _d_ya, _d_xi, _d_yi, _av, _av_xa, _av_ya, _gather, _gather_yz, _gather_xz, _gather_xy, _current
+export _av_x, _av_y, _av_z, _av_yz, _av_xz, _av_xy
+export _harm_x, _harm_y, _harm_z, _harm_yz, _harm_xz, _harm_xy
 
 include("rheology/StressUpdate.jl")
 export plastic_params, compute_dτ_r, _compute_τ_nonlinear!

a238d0f38f53f7583b510955aa339ed3bb9bf76a
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-09-28 12:07:45 +0200

    update P kernels syntax

--- src/stokes/PressureKernels.jl
+++ src/stokes/PressureKernels.jl
@@ -1,3 +1,37 @@
+# Continuity equation
+
+## Incompressible 
+@parallel_indices (I...) function compute_P!(P, RP, ∇V, η, r, θ_dτ)
+    RP[I...], P[I...] = _compute_P!(P[I...], ∇V[I...], η[I...], r, θ_dτ)
+    return nothing
+end
+
+## Compressible 
+@parallel_indices (I...) function compute_P!(P, P0, RP, ∇V, η, K, dt, r, θ_dτ)
+    RP[I...], P[I...] = _compute_P!(
+        P[I...], P0[I...], ∇V[I...], η[I...], K[I...], dt, r, θ_dτ
+    )
+    return nothing
+end
+
+# With GeoParams
+
+@parallel_indices (I...) function compute_P!(
+    P, P0, RP, ∇V, η, rheology::NTuple{N,MaterialParams}, phase, dt, r, θ_dτ
+) where {N}
+    K = get_Kb(rheology, phase[I...])
+    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
+    return nothing
+end
+
+@parallel_indices (I...) function compute_P!(
+    P, P0, RP, ∇V, η, rheology::NTuple{N,MaterialParams}, phase_ratio::C, dt, r, θ_dτ
+) where {N,C<:JustRelax.CellArray}
+    K = fn_ratio(get_Kb, rheology, phase_ratio[I...])
+    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
+    return nothing
+end
+
 # Pressure innermost kernels 
 
 function _compute_P!(P, ∇V, η, r, θ_dτ)
@@ -8,121 +42,7 @@ end
 
 function _compute_P!(P, P0, ∇V, η, K, dt, r, θ_dτ)
     _Kdt = inv(K * dt)
-    RP = -∇V - (P - P0) * _Kdt
+    RP = muladd(-(P - P0), _Kdt, -∇V)
     P += RP / (1.0 / (r / θ_dτ * η) + 1.0 * _Kdt)
     return RP, P
-end
-
-# Continuity equation
-
-## Incompressible 
-@parallel_indices (i, j) function compute_P!(
-    P::AbstractArray{T,2}, RP, ∇V, η, r, θ_dτ
-) where {T}
-    I = i, j
-    RP[I...], P[I...] = _compute_P!(P[I...], ∇V[I...], η[I...], r, θ_dτ)
-    return nothing
-end
-
-@parallel_indices (i, j, k) function compute_P!(
-    P::AbstractArray{T,3}, RP, ∇V, η, r, θ_dτ
-) where {T}
-    I = i, j, k
-    RP[I...], P[I...] = _compute_P!(P[I...], ∇V[I...], η[I...], r, θ_dτ)
-    return nothing
-end
-
-## Compressible 
-@parallel_indices (i, j) function compute_P!(
-    P::AbstractArray{T,2}, P0, RP, ∇V, η, K, dt, r, θ_dτ
-) where {T}
-    I = i, j
-    RP[I...], P[I...] = _compute_P!(
-        P[I...], P0[I...], ∇V[I...], η[I...], K[I...], dt, r, θ_dτ
-    )
-    return nothing
-end
-
-@parallel_indices (i, j, k) function compute_P!(
-    P::AbstractArray{T,3}, P0, RP, ∇V, η, K, dt, r, θ_dτ
-) where {T}
-    I = i, j, k
-    RP[I...], P[I...] = _compute_P!(
-        P[I...], P0[I...], ∇V[I...], η[I...], K[I...], dt, r, θ_dτ
-    )
-    return nothing
-end
-
-# With GeoParams
-
-@parallel_indices (i, j) function compute_P!(
-    P::AbstractArray{T,2},
-    P0,
-    RP,
-    ∇V,
-    η,
-    rheology::NTuple{N,MaterialParams},
-    phase,
-    dt,
-    r,
-    θ_dτ,
-) where {T,N}
-    I = i, j
-    K = get_Kb(rheology, phase[I...])
-    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
-    return nothing
-end
-
-@parallel_indices (i, j, k) function compute_P!(
-    P::AbstractArray{T,3},
-    P0,
-    RP,
-    ∇V,
-    η,
-    rheology::NTuple{N,MaterialParams},
-    phase,
-    dt,
-    r,
-    θ_dτ,
-) where {T,N}
-    I = i, j, k
-    K = get_Kb(rheology, phase[I...])
-    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
-    return nothing
-end
-
-@parallel_indices (i, j) function compute_P!(
-    P::AbstractArray{T,2},
-    P0,
-    RP,
-    ∇V,
-    η,
-    rheology::NTuple{N,MaterialParams},
-    phase_ratio::C,
-    dt,
-    r,
-    θ_dτ,
-) where {N,T,C<:JustRelax.CellArray}
-    I = i, j
-    K = fn_ratio(get_Kb, rheology, phase_ratio[I...])
-    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
-    return nothing
-end
-
-@parallel_indices (i, j, k) function compute_P!(
-    P::AbstractArray{T,3},
-    P0,
-    RP,
-    ∇V,
-    η,
-    rheology::NTuple{N,MaterialParams},
-    phase_ratio::C,
-    dt,
-    r,
-    θ_dτ,
-) where {N,T,C<:JustRelax.CellArray}
-    I = i, j, k
-    K = fn_ratio(get_Kb, rheology, phase_ratio[I...])
-    RP[I...], P[I...] = _compute_P!(P[I...], P0[I...], ∇V[I...], η[I...], K, dt, r, θ_dτ)
-    return nothing
 end

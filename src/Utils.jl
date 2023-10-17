4c471c8982d70d8928c811925b8dcb53e57376a2
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-10-17 15:47:05 +0200

    rework @idx

--- src/Utils.jl
+++ src/Utils.jl
@@ -6,11 +6,12 @@ Make a linear range from `1` to `args[i]`, with `i ∈ [1, ..., n]`
 """
 macro idx(args...)
     return quote
-        _idx(tuple($(esc.(args)...))...)
+        _idx($(esc.(args)...))
     end
 end
 
 @inline _idx(args::NTuple{N,Int}) where {N} = ntuple(i -> 1:args[i], Val(N))
+@inline _idx(args::Vararg{Int,N}) where {N} = ntuple(i -> 1:args[i], Val(N))
 
 """
     copy(B, A)

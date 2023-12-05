116aa8fbcf9f43446d96cba88c55fcd6395573c5
Pascal Aellig <pascal.aellig@hotmail.com>
2023-12-05 16:07:57 +0100

    shearheating funciton

--- src/thermal_diffusion/Shearheating.jl
+++ src/thermal_diffusion/Shearheating.jl
@@ -1,6 +1,3 @@
-using JustRelax
-# geoparams SetMaterialParams has a name/struct for shearheating - check it out
-
 @parallel_indices (I...) function compute_SH!(
     H,
     τ,
@@ -15,18 +12,13 @@ using JustRelax
     phase = @inbounds phase_ratios[I...]
     _Gdt = inv(fn_ratio(get_G, rheology, phase) * dt)
 
-
     τij, τij_o, εij = JustRelax.cache_tensors(τ, τ_old, ε, I...)
     τII[I...] = τII_ij = second_invariant(τij...)
     τII_old[I...] = τII_ij_old = second_invariant(τij_o...)
 
-    εij_el = if !isinf(_Gdt)
-        0.5 * ((τII_ij - τII_ij_old) * _Gdt)
-    else
-        zero(eltype(τ[1]))
-    end
+    εij_el = 0.5 * ((τII_ij - τII_ij_old) * _Gdt)
 
-    H[I...] = compute_shearheating(rheology, phase_ratios, (τ=τij, ε=εij, ε_el=εij_el))
+    H[I...] = compute_shearheating(rheology, phase_ratios, τij, εij, εij_el)
     return nothing
 end
 
@@ -34,14 +26,14 @@ end
 
 
 
-@parallel (@idx ni) compute_SH!(
-    thermal.H,
-    @tensor_center(stokes.τ),
-    stokes.τ.II,
-    @tensor_center(stokes.τ_o),
-    stokes.τ_o.II,
-    @strain(stokes),
-    phase_ratios.center,
-    tupleize(rheology), # needs to be a tuple
-    dt,
-)
+# @parallel (@idx ni) compute_SH!(
+#     thermal.H,
+#     @tensor_center(stokes.τ),
+#     stokes.τ.II,
+#     @tensor_center(stokes.τ_o),
+#     stokes.τ_o.II,
+#     @strain(stokes),
+#     phase_ratios.center,
+#     tupleize(rheology), # needs to be a tuple
+#     dt,
+# )

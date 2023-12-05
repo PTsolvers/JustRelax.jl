b253ac22b4231601250fb243734abf6f72ccd5cc
Pascal Aellig <pascal.aellig@hotmail.com>
2023-12-05 17:35:30 +0100

    SH function

--- src/thermal_diffusion/Shearheating.jl
+++ src/thermal_diffusion/Shearheating.jl
@@ -5,20 +5,19 @@
     τ_old,
     τII_old,
     ε,
-    phase_ratios, #phase_v is passed onto thermal solver temperature2center to average? will probably update with phase_ratios
+    phase_ratios,
     rheology,
     dt,
     )
+
     phase = @inbounds phase_ratios[I...]
     _Gdt = inv(fn_ratio(get_G, rheology, phase) * dt)
-
     τij, τij_o, εij = JustRelax.cache_tensors(τ, τ_old, ε, I...)
     τII[I...] = τII_ij = second_invariant(τij...)
     τII_old[I...] = τII_ij_old = second_invariant(τij_o...)
 
     εij_el = 0.5 * ((τII_ij - τII_ij_old) * _Gdt)
-
-    H[I...] = compute_shearheating(rheology, phase_ratios, τij, εij, εij_el)
+    H[I...] = H_s = compute_shearheating(rheology, phase, τij, εij, εij_el) # @AdM - i'm not sure about the H[I...] = H_s part
     return nothing
 end
 
@@ -34,6 +33,6 @@ end
 #     stokes.τ_o.II,
 #     @strain(stokes),
 #     phase_ratios.center,
-#     tupleize(rheology), # needs to be a tuple
+#     rheology,
 #     dt,
 # )

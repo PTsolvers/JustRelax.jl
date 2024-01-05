1de283016186f4d5454505e68e0d26893e5109b5
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2024-01-05 19:30:13 +0100

    Fix  shear heating bug (#86)
    Fixes bug in the shear heating kernel for single phase materials.

--- src/thermal_diffusion/Shearheating.jl
+++ src/thermal_diffusion/Shearheating.jl
@@ -1,7 +1,7 @@
 @parallel_indices (I...) function compute_shear_heating!(
     shear_heating, τ::NTuple{N,T}, τ_old::NTuple{N,T}, ε::NTuple{N,T}, rheology, dt
 ) where {N,T}
-    _Gdt = inv(fn_ratio(get_G, rheology) * dt)
+    _Gdt = inv(get_G(rheology) * dt)
     τij, τij_o, εij = cache_tensors(τ, τ_old, ε, I...)
     εij_el = @. 0.5 * ((τij - τij_o) * _Gdt)
     shear_heating[I...] = compute_shearheating(rheology, τij, εij, εij_el)

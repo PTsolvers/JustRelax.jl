acd3a10ab82c3d58fd36c64ab99d53c428a8bc20
Pascal Aellig <pascal.aellig@hotmail.com>
2024-02-19 15:34:14 +0100

    add shearheating 3D tests

--- test/test_shearheating2D.jl
+++ test/test_shearheating2D.jl
@@ -254,6 +254,6 @@ end
 
 @testset "Shearheating2D" begin
     iters, thermal = Shearheating2D()
-    @test passed = iters.err_evo1[end] < 1e-5
+    @test passed = iters.err_evo1[end] < 1e-4
     # @test maximum.(thermal.shear_heating)
 end

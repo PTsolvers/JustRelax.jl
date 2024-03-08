22506e53a52171a5c094fc45ca8441f969465a1b
aelligp <pascal.aellig@hotmail.com>
2024-03-08 23:07:46 +0100

    add PR comment; test supressor

--- test/test_CellArrays2D.jl
+++ test/test_CellArrays2D.jl
@@ -6,7 +6,7 @@ model = PS_Setup(:Threads, Float64, 2)
 environment!(model)
 
 @testset "CellArrays 2D" begin
-    @suppress begin
+    # @suppress begin
         ni = 5, 5
         A  = JustRelax.@fill(false, ni..., celldims=(2,), eltype=Bool)
 
@@ -20,7 +20,7 @@ environment!(model)
         @cell A[1, 1, 1] = true
         @test @cell(A[1, 1, 1]) === true
         @test (@allocated @cell A[1, 1, 1] = true) === 0
-    end
+    # end
     # @test A[1, 1] === SA[true, false]
     # allocs = check_allocs(getindex, (typeof(A), Int64, Int64))
     # @test isempty(allocs)

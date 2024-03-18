b1ae1851386cecd9593f18df2ee34a66acf5a81b
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-03-18 22:28:57 +0100

    fix get_thermal_expansion

--- src/rheology/GeoParams.jl
+++ src/rheology/GeoParams.jl
@@ -14,10 +14,14 @@ function get_shear_modulus(args...)
     return Kb
 end
 
-function get_thermal_expansion(args...)
-    α = GeoParams.get_α(args...)
+function get_thermal_expansion(p)
+    α = get_α(p)
     if isnan(α) || iszero(α)
         return 0.0
     end
     return α
 end
+
+@inline get_α(p::MaterialParams) = get_α(p.Density[1])
+@inline get_α(p::ConstantDensity) = 0.0
+@inline get_α(p::Union{T_Density, PT_Density}) = GeoParams.get_α(p)

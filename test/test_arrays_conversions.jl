af2348f6ec9d9e0c10e906859cd77cab4fa8e3e7
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-04-27 10:01:46 +0200

    expand test suit

--- test/test_arrays_conversions.jl
+++ test/test_arrays_conversions.jl
@@ -7,10 +7,18 @@ environment!(model)
     stokes  = StokesArrays(ni, ViscoElastic)
     thermal = ThermalArrays(ni)
 
-    @test Array(stokes.V) isa Velocity{Matrix{Float64}}
-    @test Array(stokes.τ) isa SymmetricTensor{Matrix{Float64}}
-    @test Array(stokes.R) isa Residual{Matrix{Float64}}
-    @test Array(stokes.P) isa Matrix
+    @test Array(stokes.V) isa Velocity{Array{T, N}} where {T, N}
+    @test Array(stokes.τ) isa SymmetricTensor{Array{T, N}} where {T, N}
+    @test Array(stokes.R) isa Residual{Array{T, N}} where {T, N}
+    @test Array(stokes.P) isa Array{T, N} where {T, N}
     @test Array(stokes)   isa StokesArrays
-    @test Array(thermal)  isa ThermalArrays{Matrix{Float64}}
+    @test Array(thermal)  isa ThermalArrays{Array{T, N}} where {T, N}
+    
+    @test JustRelax.iscpu(stokes.V) isa JustRelax.CPUDeviceTrait
+    @test JustRelax.iscpu(stokes.τ) isa JustRelax.CPUDeviceTrait
+    @test JustRelax.iscpu(stokes.R) isa JustRelax.CPUDeviceTrait
+    @test JustRelax.iscpu(stokes.P) isa JustRelax.CPUDeviceTrait
+    @test JustRelax.iscpu(stokes)   isa JustRelax.CPUDeviceTrait
+    @test JustRelax.iscpu(thermal)  isa JustRelax.CPUDeviceTrait
+    @test_throws ArgumentError("Unkown device") JustRelax.iscpu("potato")
 end

ba642ea838fa31ed14d2dd9a5e2bb48a4a325b4f
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-04-27 10:01:37 +0200

    improve Array conversions

--- src/array_conversions.jl
+++ src/array_conversions.jl
@@ -1,9 +1,39 @@
+# Device trait system 
 
-import Base.Array
+abstract type DeviceTrait end
+struct CPUDeviceTrait <: DeviceTrait end
+struct NonCPUDeviceTrait <: DeviceTrait end
+
+@inline iscpu(::Array) = CPUDeviceTrait()
+@inline iscpu(::AbstractArray) = NonCPUDeviceTrait()
+@inline iscpu(::T) where T = throw(ArgumentError("Unkown device"))
+
+@inline iscpu(::Velocity{Array{T, N}}) where {T, N} = CPUDeviceTrait()
+@inline iscpu(::Velocity{AbstractArray{T, N}}) where {T, N} = NonCPUDeviceTrait()
+
+@inline iscpu(::SymmetricTensor{Array{T, N}}) where {T, N} = CPUDeviceTrait()
+@inline iscpu(::SymmetricTensor{AbstractArray{T, N}}) where {T, N} = NonCPUDeviceTrait()
+
+@inline iscpu(::Residual{Array{T, N}}) where {T, N} = CPUDeviceTrait()
+@inline iscpu(::Residual{AbstractArray{T, N}}) where {T, N} = NonCPUDeviceTrait()
+
+@inline iscpu(::ThermalArrays{Array{T, N}}) where {T, N} = CPUDeviceTrait()
+@inline iscpu(::ThermalArrays{AbstractArray{T, N}}) where {T, N} = NonCPUDeviceTrait()
+
+@inline iscpu(::StokesArrays{M,A,B,C,Array{T, N},nDim}) where {M,A,B,C,T,N,nDim} =CPUDeviceTrait()
+@inline iscpu(::StokesArrays{M,A,B,C,AbstractArray{T, N},nDim}) where {M,A,B,C,T,N,nDim} =NonCPUDeviceTrait()
+
+## Conversion of structs to CPU
 
 @inline remove_parameters(::T) where T = Base.typename(T).wrapper
 
-function Array(x::T) where T<:Union{SymmetricTensor, ThermalArrays, Velocity, Residual}
+function Array(x::T) where T<:Union{StokesArrays, SymmetricTensor, ThermalArrays, Velocity, Residual}
+    Array(iscpu(x), x)
+end
+
+Array(::CPUDeviceTrait, x) = x
+
+function Array(::NonCPUDeviceTrait, x::T) where T<:Union{SymmetricTensor, ThermalArrays, Velocity, Residual}
     nfields = fieldcount(T)
     cpu_fields = ntuple(Val(nfields)) do i
         Base.@_inline_meta
@@ -13,7 +43,7 @@ function Array(x::T) where T<:Union{SymmetricTensor, ThermalArrays, Velocity, Re
     return T_clean(cpu_fields...)
 end
 
-function Array(x::StokesArrays{T,A,B,C,M,nDim}) where {T,A,B,C,M,nDim}
+function Array(::NonCPUDeviceTrait, x::StokesArrays{T,A,B,C,M,nDim}) where {T,A,B,C,M,nDim}
     nfields = fieldcount(StokesArrays)
     cpu_fields = ntuple(Val(nfields)) do i
         Base.@_inline_meta

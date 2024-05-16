e321be0f492967f2c1611354f9d4ff916571d3cd
albert-de-montserrat <albert.de.montserrat@gmail.com>
2024-05-16 14:05:03 +0200

    fix geotherm

--- subduction/Buiter/Buiter_setup2D_sticky.jl
+++ subduction/Buiter/Buiter_setup2D_sticky.jl
@@ -6,12 +6,13 @@ function GMG_subduction_2D(nx, ny)
     nx, nz = nx, ny
     Tbot   = 1474.0 
     x      = range(0, 3000, nx);
-    z      = range(-model_depth, 10, nz);
+    air_thickness = 10.0
+    z      = range(-model_depth, air_thickness, nz);
     Grid2D = CartData(xyz_grid(x,0,z))
     Phases = zeros(Int64, nx, 1, nz);
     Temp   = fill(Tbot, nx, 1, nz);
-    air_thickness = 20.0
-    lith   = LithosphericPhases(Layers=[80], Phases=[1 0])
+    Tlab   = 1300
+    # lith   = LithosphericPhases(Layers=[80], Phases=[1 0], Tlab=Tlab)
   
     # phases
     # 0: asthenosphere
@@ -26,8 +27,8 @@ function GMG_subduction_2D(nx, ny)
         xlim    =(0, 3000),
         zlim    =(-model_depth, 0.0), 
         Origin  = nothing, StrikeAngle=0, DipAngle=0,
-        phase   = LithosphericPhases(Layers=[], Phases=[0]), 
-        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=80, Adiabat=0.4)
+        phase   = LithosphericPhases(Layers=[], Phases=[0], Tlab=Tlab), 
+        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=50, Adiabat=0)
     )
 
     # Add left oceanic plate
@@ -38,8 +39,8 @@ function GMG_subduction_2D(nx, ny)
         xlim    =(100, 3000-100),
         zlim    =(-model_depth, 0.0), 
         Origin  = nothing, StrikeAngle=0, DipAngle=0,
-        phase   = lith, 
-        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=80, Adiabat=0.4)
+        phase   = LithosphericPhases(Layers=[80], Phases=[1 0], Tlab=Tlab), 
+        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=50, Adiabat=0)
     )
 
     # Add right oceanic plate crust
@@ -50,8 +51,8 @@ function GMG_subduction_2D(nx, ny)
         xlim    =(3000-1430, 3000-200), 
         zlim    =(-model_depth, 0.0), 
         Origin  = nothing, StrikeAngle=0, DipAngle=0,
-        phase   = LithosphericPhases(Layers=[8 72], Phases=[2 1 0]), 
-        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=80, Adiabat=0.4)
+        phase   = LithosphericPhases(Layers=[8 72], Phases=[2 1 0], Tlab=Tlab), 
+        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=50, Adiabat=0)
     )
 
     # Add slab
@@ -62,17 +63,9 @@ function GMG_subduction_2D(nx, ny)
         xlim    = (3000-1430, 3000-1430-250), 
         zlim    =(-80, 0.0), 
         Origin  = nothing, StrikeAngle=0, DipAngle=-30,
-        phase   = LithosphericPhases(Layers=[8 80], Phases=[2 1 0]), #, Tlab=Tbot ), 
-        T       = HalfspaceCoolingTemp(
-            Tsurface = 20,
-            Tmantle  = Tbot,
-            Age      = 80,
-        ) 
+        phase   = LithosphericPhases(Layers=[8 80], Phases=[2 1 0], Tlab=Tlab), 
+        T       = HalfspaceCoolingTemp(Tsurface=20, Tmantle=Tbot, Age=50, Adiabat=0)
     )
-    heatmap(x,z,Temp[:,1,:])
-    # Lithosphere-asthenosphere boundary:
-    # ind = findall(Temp .> 1250 .&& (Phases.==2 .|| Phases.==5));
-    # Phases[ind] .= 0;
 
     surf = Grid2D.z.val .> 0.0 
     Temp[surf] .= 20.0
@@ -83,7 +76,8 @@ function GMG_subduction_2D(nx, ny)
     li = (abs(last(x)-first(x)), abs(last(z)-first(z))).* 1e3
     origin = (x[1], z[1]) .* 1e3
 
-    ph       = Phases[:,1,:] .+ 1
+    ph      = Phases[:,1,:] .+ 1
+    T       = Temp[:,1,:]
 
-    return li, origin, ph, Temp[:,1,:].+273
+    return li, origin, ph, T
 end

5bb0e080808c0e0c36d815b2437ae9b43a2cb71f
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-05-24 17:19:43 +0200

    simplify XArray selection

--- test/test_types.jl
+++ test/test_types.jl
@@ -18,13 +18,7 @@ else
     CPUBackend
 end
 
-A = @static if env_backend === "AMDGPU"
-    ROCArray
-elseif env_backend === "CUDA"
-    CuArray
-else
-    Array
-end
+const BackendArray = PTArray(backend)
 
 @testset "2D allocators" begin
     ni = nx, ny = (2, 2)
@@ -35,18 +29,18 @@ end
     @test size(R.Rx) == (nx-1, ny)
     @test size(R.Ry) == (nx, ny-1)
     @test size(R.RP) == ni
-    @test R.Rx isa A
-    @test R.Ry isa A
-    @test R.RP isa A
+    @test R.Rx isa BackendArray
+    @test R.Ry isa BackendArray
+    @test R.RP isa BackendArray
     @test_throws MethodError JR2.Residual(10.0, 10.0)
 
     visc = JR2.Viscosity(ni)
     @test size(visc.η)     == ni
     @test size(visc.η_vep) == ni
     @test size(visc.ητ)    == ni
-    @test visc.η     isa A
-    @test visc.η_vep isa A
-    @test visc.ητ    isa A
+    @test visc.η     isa BackendArray
+    @test visc.η_vep isa BackendArray
+    @test visc.ητ    isa BackendArray
     @test_throws MethodError JR2.Viscosity(10.0, 10.0)
 
     v      = JR2.Velocity(ni...)
@@ -58,11 +52,11 @@ end
     @test size(tensor.xy_c) == (nx, ny)
     @test size(tensor.II)   == (nx, ny)
 
-    @test tensor.xx   isa A
-    @test tensor.yy   isa A
-    @test tensor.xy   isa A
-    @test tensor.xy_c isa A
-    @test tensor.II   isa A
+    @test tensor.xx   isa BackendArray
+    @test tensor.yy   isa BackendArray
+    @test tensor.xy   isa BackendArray
+    @test tensor.xy_c isa BackendArray
+    @test tensor.II   isa BackendArray
 
     stokes = JR2.StokesArrays(backend, ni)
 
@@ -71,15 +65,15 @@ end
     @test size(stokes.∇V)     == ni
     @test size(stokes.EII_pl) == ni
 
-    @test stokes.P          isa A
-    @test stokes.P0         isa A
-    @test stokes.∇V         isa A
+    @test stokes.P          isa BackendArray
+    @test stokes.P0         isa BackendArray
+    @test stokes.∇V         isa BackendArray
     @test stokes.V          isa JustRelax.Velocity
     @test stokes.τ          isa JustRelax.SymmetricTensor
     @test stokes.τ_o        isa JustRelax.SymmetricTensor
     @test stokes.ε          isa JustRelax.SymmetricTensor
     @test stokes.ε_pl       isa JustRelax.SymmetricTensor
-    @test stokes.EII_pl     isa A
+    @test stokes.EII_pl     isa BackendArray
     @test stokes.viscosity  isa JustRelax.Viscosity
     @test stokes.R          isa JustRelax.Residual
 
@@ -95,19 +89,19 @@ end
     @test size(R.Ry) == (nx, ny-1, nz)
     @test size(R.Rz) == (nx, ny, nz-1)
     @test size(R.RP) == ni
-    @test R.Rx isa A
-    @test R.Ry isa A
-    @test R.Rz isa A
-    @test R.RP isa A
+    @test R.Rx isa BackendArray
+    @test R.Ry isa BackendArray
+    @test R.Rz isa BackendArray
+    @test R.RP isa BackendArray
     @test_throws MethodError JR3.Residual(1.0, 1.0, 1.0)
 
     visc = JR3.Viscosity(ni)
     @test size(visc.η)     == ni
     @test size(visc.η_vep) == ni
     @test size(visc.ητ)    == ni
-    @test visc.η     isa A
-    @test visc.η_vep isa A
-    @test visc.ητ    isa A
+    @test visc.η     isa BackendArray
+    @test visc.η_vep isa BackendArray
+    @test visc.ητ    isa BackendArray
     @test_throws MethodError JR3.Viscosity(1.0, 1.0, 1.0)
 
     v      = JR3.Velocity(ni...)
@@ -123,15 +117,15 @@ end
     @test size(tensor.xz_c) == ni
     @test size(tensor.II)   == ni
 
-    @test tensor.xx   isa A
-    @test tensor.yy   isa A
-    @test tensor.xy   isa A
-    @test tensor.yz   isa A
-    @test tensor.xz   isa A
-    @test tensor.xy_c isa A
-    @test tensor.yz_c isa A
-    @test tensor.xz_c isa A
-    @test tensor.II   isa A
+    @test tensor.xx   isa BackendArray
+    @test tensor.yy   isa BackendArray
+    @test tensor.xy   isa BackendArray
+    @test tensor.yz   isa BackendArray
+    @test tensor.xz   isa BackendArray
+    @test tensor.xy_c isa BackendArray
+    @test tensor.yz_c isa BackendArray
+    @test tensor.xz_c isa BackendArray
+    @test tensor.II   isa BackendArray
 
     stokes = JR3.StokesArrays(backend, ni)
 
@@ -140,15 +134,15 @@ end
     @test size(stokes.∇V)     == ni
     @test size(stokes.EII_pl) == ni
 
-    @test stokes.P          isa A
-    @test stokes.P0         isa A
-    @test stokes.∇V         isa A
+    @test stokes.P          isa BackendArray
+    @test stokes.P0         isa BackendArray
+    @test stokes.∇V         isa BackendArray
     @test stokes.V          isa JustRelax.Velocity
     @test stokes.τ          isa JustRelax.SymmetricTensor
     @test stokes.τ_o        isa JustRelax.SymmetricTensor
     @test stokes.ε          isa JustRelax.SymmetricTensor
     @test stokes.ε_pl       isa JustRelax.SymmetricTensor
-    @test stokes.EII_pl     isa A
+    @test stokes.EII_pl     isa BackendArray
     @test stokes.viscosity  isa JustRelax.Viscosity
     @test stokes.R          isa JustRelax.Residual
 

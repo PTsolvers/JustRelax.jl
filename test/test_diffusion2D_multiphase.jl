7aa750dd0428714c56aca3f22b173562f3deb4ab
Pascal Aellig <pascal.aellig@hotmail.com>
2024-05-24 13:55:45 +0200

    fix missed PS init

--- test/test_diffusion2D_multiphase.jl
+++ test/test_diffusion2D_multiphase.jl
@@ -11,6 +11,7 @@ end
 using Test, Suppressor
 using GeoParams
 using JustRelax, JustRelax.JustRelax2D
+using ParallelStencil
 
 const backend_JR = @static if ENV["JULIA_JUSTRELAX_BACKEND"] === "AMDGPU"
     AMDGPUBackend
@@ -21,10 +22,8 @@ else
 end
 
 @static if ENV["JULIA_JUSTRELAX_BACKEND"] === "AMDGPU"
-    using ParallelStencil
     @init_parallel_stencil(AMDGPU, Float64, 2)
 elseif ENV["JULIA_JUSTRELAX_BACKEND"] === "CUDA"
-    using ParallelStencil
     @init_parallel_stencil(CUDA, Float64, 2)
 else
     @init_parallel_stencil(Threads, Float64, 2)

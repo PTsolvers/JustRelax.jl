ba95fbb5850789482535cc7538f8fecf04a53eaf
Pascal Aellig <pascal.aellig@hotmail.com>
2024-05-24 13:43:47 +0200

    directly import `CUDA/AMDGPUBackend`

--- test/test_traits.jl
+++ test/test_traits.jl
@@ -9,6 +9,8 @@ end
 using JustRelax, Test
 import JustRelax.JustRelax2D as JR2
 import JustRelax.JustRelax3D as JR3
+import JR2.AMDGPUBackend, JR2.CUDABackend
+import JR3.AMDGPUBackend, JR3.CUDABackend
 
 const bk = JustRelax.backend
 

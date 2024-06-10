9dfe601fdeb63264eb1159d8cfc9592e5704f322
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2024-06-10 19:40:47 +0200

    update 3D shear band localisation miniapps (#177)

--- miniapps/benchmarks/stokes3D/shear_band/ShearBand3D.jl
+++ miniapps/benchmarks/stokes3D/shear_band/ShearBand3D.jl
@@ -106,7 +106,8 @@ function main(igg; nx=64, ny=64, nz=64, figdir="model_figs")
     stokes.V.Vy .= PTArray([ y*εbg/2 for _ in 1:nx+2, y in xvi[2], _ in 1:nz+2])
     stokes.V.Vz .= PTArray([-z*εbg   for _ in 1:nx+2, _ in 1:nx+2, z in xvi[3]])
     flow_bcs!(stokes, flow_bcs) # apply boundary conditions
-    update_halo!(stokes.V.Vx, stokes.V.Vy, stokes.V.Vz)
+    update_halo!(@velocity(stokes)...)
+    
     # IO ------------------------------------------------
     # if it does not exist, make folder where figures are stored
     !isdir(figdir) && mkpath(figdir)
@@ -139,8 +140,7 @@ function main(igg; nx=64, ny=64, nz=64, figdir="model_figs")
             )
         )
 
-
-        tensor_invariant!(stokes.ε.II, @strain(stokes)...)
+        tensor_invariant!(stokes.ε)
         push!(τII, maximum(stokes.τ.xx))
 
         it += 1

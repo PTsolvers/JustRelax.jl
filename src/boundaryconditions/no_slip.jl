7138c8fe3cec157d0a22a872c29273e66c7709f0
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-07-03 17:01:20 +0200

    fix no_slip in 3D

--- src/boundaryconditions/no_slip.jl
+++ src/boundaryconditions/no_slip.jl
@@ -27,12 +27,10 @@ end
 @parallel_indices (i, j) function no_slip!(Ax, Ay, Az, bc)
     @inbounds begin
         if bc.left
-            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[1, i, j] = 0.0)
             (1 < i < size(Ay, 2)) && (1 < j < size(Ay, 3)) && (Ay[1, i, j] = -Ay[2, i, j])
             (1 < i < size(Az, 2)) && (1 < j < size(Az, 3)) && (Az[1, i, j] = -Az[2, i, j])
         end
         if bc.right
-            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[end, i, j] = 0.0)
             (1 < i < size(Ay, 2)) &&
                 (1 < j < size(Ay, 3)) &&
                 (Ay[end, i, j] = -Ay[end - 1, i, j])
@@ -42,12 +40,10 @@ end
         end
 
         if bc.front
-            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 2)) && (Ay[i, 1, j] = 0.0)
             (1 < i < size(Ax, 1)) && (1 < j < size(Ax, 3)) && (Ax[i, 1, j] = -Ax[i, 2, j])
             (1 < i < size(Az, 1)) && (1 < j < size(Az, 3)) && (Az[i, 1, j] = -Az[i, 2, j])
         end
         if bc.back
-            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 2)) && (Ay[i, end, j] = 0.0)
             (1 < i < size(Ax, 1)) &&
                 (1 < j < size(Ax, 3)) &&
                 (Ax[i, end, j] = -Ax[i, end - 1, j])
@@ -57,12 +53,10 @@ end
         end
 
         if bc.bot
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, j, 1] = 0.0)
             (1 < i < size(Ax, 1)) && (1 < j < size(Ax, 2)) && (Ax[i, j, 1] = -Ax[i, j, 2])
             (1 < i < size(Ay, 1)) && (1 < j < size(Ay, 2)) && (Ay[i, j, 1] = -Ay[i, j, 2])
         end
         if bc.top
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, j, end] = 0.0)
             (1 < i < size(Ax, 1)) &&
                 (1 < j < size(Ax, 2)) &&
                 (Ax[i, j, end] = -Ax[i, j, end - 1])
@@ -71,6 +65,27 @@ end
                 (Ay[i, j, end] = -Ay[i, j, end - 1])
         end
 
+        ##
+        if bc.left
+            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[1, i, j] = 0.0)
+        end
+        if bc.right
+            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[end, i, j] = 0.0)
+        end
+
+        if bc.front
+            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, 1, j] = 0.0)
+        end
+        if bc.back
+            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, end, j] = 0.0)
+        end
+
+        if bc.bot
+            (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, 1] = 0.0)
+        end
+        if bc.top
+            (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, end] = 0.0)
+        end
         # corners
         # bc.bot && (Ax[1, 1] = 0.0; Ax[1, 1] = 0.0)
         # bc.left && bc.bot && (Ax[1, 1] = 0.0)

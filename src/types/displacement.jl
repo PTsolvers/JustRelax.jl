555cae101b999e6fb4b5ebbe5a854ace6595126c
Pascal Aellig <pascal.aellig@hotmail.com>
2024-07-03 17:15:43 +0200

    fix bug

--- src/types/displacement.jl
+++ src/types/displacement.jl
@@ -11,20 +11,20 @@ function _velocity2displacement!(stokes::JustRelax.StokesArrays, dt)
     ni = size(stokes.P)
     (; V, U) = stokes
     @parallel (@idx ni .+ 2) _velocity2displacement!(
-        V.Vx, V.Vy, V.Vz, U.Ux, U.Uy, U.Uz, 1 / dt
+        V.Vx, V.Vy, V.Vz, U.Ux, U.Uy, U.Uz, dt
     )
     return nothing
 end
 
-@parallel_indices (I...) function _velocity2displacement!(Vx, Vy, Vz, Ux, Uy, Uz, _dt)
+@parallel_indices (I...) function _velocity2displacement!(Vx, Vy, Vz, Ux, Uy, Uz, dt)
     if all(I .≤ size(Ux))
-        Ux[I...] = Vx[I...] * _dt
+        Ux[I...] = Vx[I...] * dt
     end
     if all(I .≤ size(Uy))
-        Uy[I...] = Vy[I...] * _dt
+        Uy[I...] = Vy[I...] * dt
     end
     if !isnothing(Vz) && all(I .≤ size(Uz))
-        Uz[I...] = Vz[I...] * _dt
+        Uz[I...] = Vz[I...] * dt
     end
     return nothing
 end
@@ -41,19 +41,19 @@ end
 function _displacement2velocity!(stokes::JustRelax.StokesArrays, dt)
     ni = size(stokes.P)
     (; V, U) = stokes
-    @parallel (@idx ni .+ 2) _displacement2velocity!(U.Ux, U.Uy, U.Uz, V.Vx, V.Vy, V.Vz, dt)
+    @parallel (@idx ni .+ 2) _displacement2velocity!(U.Ux, U.Uy, U.Uz, V.Vx, V.Vy, V.Vz, 1 / dt)
     return nothing
 end
 
-@parallel_indices (I...) function _displacement2velocity!(Ux, Uy, Uz, Vx, Vy, Vz, dt)
+@parallel_indices (I...) function _displacement2velocity!(Ux, Uy, Uz, Vx, Vy, Vz, _dt)
     if all(I .≤ size(Ux))
-        Vx[I...] = Ux[I...] * dt
+        Vx[I...] = Ux[I...] * _dt
     end
     if all(I .≤ size(Uy))
-        Vy[I...] = Uy[I...] * dt
+        Vy[I...] = Uy[I...] * _dt
     end
     if !isnothing(Vz) && all(I .≤ size(Uz))
-        Vz[I...] = Uz[I...] * dt
+        Vz[I...] = Uz[I...] * _dt
     end
     return nothing
 end

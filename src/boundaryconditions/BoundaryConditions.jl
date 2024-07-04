a23128f6eb83b52875ff0c7f7b82c81a8c7de5f0
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-07-04 20:09:41 +0200

    fix again

--- src/boundaryconditions/BoundaryConditions.jl
+++ src/boundaryconditions/BoundaryConditions.jl
@@ -55,8 +55,9 @@ function _flow_bcs!(bcs, V)
     # no slip boundary conditions
     # do_bc(bcs.no_slip) && (@parallel (@idx n) no_slip!(V..., bcs.no_slip))
     if do_bc(bcs.no_slip)
-        @parallel (@idx n) no_slip1!(V..., bcs.no_slip)
-        @parallel (@idx n) no_slip2!(V..., bcs.no_slip)
+        # @parallel (@idx n) no_slip1!(V..., bcs.no_slip)
+        # @parallel (@idx n) no_slip2!(V..., bcs.no_slip)
+        no_slip!(V..., bcs.no_slip)
     end
     # free slip boundary conditions
     do_bc(bcs.free_slip) && (@parallel (@idx n) free_slip!(V..., bcs.free_slip))
--- src/boundaryconditions/no_slip.jl
+++ src/boundaryconditions/no_slip.jl
@@ -1,94 +1,149 @@
-@parallel_indices (i) function no_slip2!(Ax, Ay, bc)
-    @inbounds begin
-        if bc.left
-            (1 < i < size(Ay, 2)) && (Ay[1, i] = -Ay[2, i])
-        end
-        if bc.right
-            (1 < i < size(Ay, 2)) && (Ay[end, i] = -Ay[end - 1, i])
-        end
-        if bc.bot
-            (1 < i < size(Ax, 1)) && (Ax[i, 1] = -Ax[i, 2])
-        end
-        if bc.top
-            (1 < i < size(Ax, 1)) && (Ax[i, end] = -Ax[i, end - 1])
-        end
+# @parallel_indices (i) function no_slip2!(Ax, Ay, bc)
+#     @inbounds begin
+#         if bc.left
+#             (1 < i < size(Ay, 2)) && (Ay[1, i] = -Ay[2, i])
+#         end
+#         if bc.right
+#             (1 < i < size(Ay, 2)) && (Ay[end, i] = -Ay[end - 1, i])
+#         end
+#         if bc.bot
+#             (1 < i < size(Ax, 1)) && (Ax[i, 1] = -Ax[i, 2])
+#         end
+#         if bc.top
+#             (1 < i < size(Ax, 1)) && (Ax[i, end] = -Ax[i, end - 1])
+#         end
+#     end
+#     return nothing
+# end
+
+# @parallel_indices (i) function no_slip1!(Ax, Ay, bc)
+#     @inbounds begin
+#         if bc.left
+#             (i ≤ size(Ax, 2)) && (Ax[1, i] = 0.0)
+#         end
+#         if bc.right
+#             (i ≤ size(Ax, 2)) && (Ax[end, i] = 0.0)
+#         end
+#         if bc.bot
+#             (i ≤ size(Ay, 1)) && (Ay[i, 1] = 0.0)
+#         end
+#         if bc.top
+#             (i ≤ size(Ay, 1)) && (Ay[i, end] = 0.0)
+#         end
+#     end
+#     return nothing
+# end
+
+# @parallel_indices (i, j) function no_slip2!(Ax, Ay, Az, bc)
+#     # @inbounds begin
+#         if bc.left
+#             (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[1, i, j] = -Ay[2, i, j])
+#             (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[1, i, j] = -Az[2, i, j])
+#         end
+#         if bc.right
+#             (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[end, i, j] = -Ay[end - 1, i, j])
+#             (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[end, i, j] = -Az[end - 1, i, j])
+#         end
+
+#         if bc.front
+#             (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, 1, j] = -Ax[i, 2, j])
+#             (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, 1, j] = -Az[i, 2, j])
+#         end
+#         if bc.back
+#             (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, end, j] = -Ax[i, end - 1, j])
+#             (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, end, j] = -Az[i, end - 1, j])
+#         end
+
+#         if bc.bot
+#             (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, 1] = -Ax[i, j, 2])
+#             (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, 1] = -Ay[i, j, 2])
+#         end
+#         if bc.top
+#             (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, end] = -Ax[i, j, end - 1])
+#             (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, end] = -Ay[i, j, end - 1])
+#         end
+#     # end
+#     return nothing
+# end
+
+# @parallel_indices (i, j) function no_slip1!(Ax, Ay, Az, bc)
+#     # @inbounds begin
+#         ##
+#         if bc.left
+#             (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[1, i, j] = 0.0)
+#         end
+#         if bc.right
+#             (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[end, i, j] = 0.0)
+#         end
+
+#         if bc.front
+#             (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, 1, j] = 0.0)
+#         end
+#         if bc.back
+#             (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, end, j] = 0.0)
+#         end
+
+#         if bc.bot
+#             (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, 1] = 0.0)
+#         end
+#         if bc.top
+#             (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, end] = 0.0)
+#         end
+#     # end
+#     return nothing
+# end
+
+@views function no_slip!(Ax, Ay, bc)
+    if bc.left
+        Ax[1, :] .= 0
+        Ay[1, :] .= -Ay[2, :]
     end
-    return nothing
-end
-
-@parallel_indices (i) function no_slip1!(Ax, Ay, bc)
-    @inbounds begin
-        if bc.left
-            (i ≤ size(Ax, 2)) && (Ax[1, i] = 0.0)
-        end
-        if bc.right
-            (i ≤ size(Ax, 2)) && (Ax[end, i] = 0.0)
-        end
-        if bc.bot
-            (i ≤ size(Ay, 1)) && (Ay[i, 1] = 0.0)
-        end
-        if bc.top
-            (i ≤ size(Ay, 1)) && (Ay[i, end] = 0.0)
-        end
+    if bc.right
+        Ax[end, :] .= 0
+        Ay[end, :] .= -Ay[end - 1, :]
     end
-    return nothing
+    if bc.bot
+        Ax[:, 1] .= -Ax[:, 2]
+        Ay[:, 1] .= -Ay[:, 2]
+    end
+    if bc.top
+        Ax[:, end] .= -Ax[:, end - 1]
+        Ay[:, end] .= -Ay[:, end - 1]
+    end
 end
 
-@parallel_indices (i, j) function no_slip2!(Ax, Ay, Az, bc)
-    # @inbounds begin
-        if bc.left
-            (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[1, i, j] = -Ay[2, i, j])
-            (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[1, i, j] = -Az[2, i, j])
-        end
-        if bc.right
-            (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[end, i, j] = -Ay[end - 1, i, j])
-            (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[end, i, j] = -Az[end - 1, i, j])
-        end
+@views function no_slip!(Ax, Ay, Az, bc)
+    if bc.left
+        Ax[1, :, :] .= 0
+        Ay[1, :, :] .= -Ay[2, :, :]
+        Az[1, :, :] .= -Az[2, :, :]
+    end
+    if bc.right
+        Ax[end, :, :] .= 0
+        Ay[end, :, :] .= -Ay[end - 1, :, :]
+        Az[end, :, :] .= -Az[end - 1, :, :]
+    end
 
-        if bc.front
-            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, 1, j] = -Ax[i, 2, j])
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, 1, j] = -Az[i, 2, j])
-        end
-        if bc.back
-            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, end, j] = -Ax[i, end - 1, j])
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, end, j] = -Az[i, end - 1, j])
-        end
+    if bc.front
+        Ax[:, 1, :] .= -Ax[:, 2, :]
+        Ay[:, 1, :] .= 0
+        Az[:, 1, :] .= -Az[:, 2, :]
+    end
 
-        if bc.bot
-            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, 1] = -Ax[i, j, 2])
-            (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, 1] = -Ay[i, j, 2])
-        end
-        if bc.top
-            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, end] = -Ax[i, j, end - 1])
-            (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, end] = -Ay[i, j, end - 1])
-        end
-    # end
-    return nothing
-end
+    if bc.back
+        Ax[:, end, :] .= -Ax[:, end - 1, :]
+        Ay[:, end, :] .= 0
+        Az[:, end, :] .= -Az[:, end - 1, :]
+    end
 
-@parallel_indices (i, j) function no_slip1!(Ax, Ay, Az, bc)
-    # @inbounds begin
-        ##
-        if bc.left
-            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[1, i, j] = 0.0)
-        end
-        if bc.right
-            (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[end, i, j] = 0.0)
-        end
-
-        if bc.front
-            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, 1, j] = 0.0)
-        end
-        if bc.back
-            (i ≤ size(Ay, 1)) && (i ≤ size(Ay, 3)) && (Ay[i, end, j] = 0.0)
-        end
-
-        if bc.bot
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, 1] = 0.0)
-        end
-        if bc.top
-            (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, end] = 0.0)
-        end
-    # end
-    return nothing
+    if bc.bot
+        Ax[:, :, 1] .= -Ax[:, :, 2]
+        Ay[:, :, 1] .= -Ay[:, :, 2]
+        Az[:, :, 1] .= 0
+    end
+    if bc.top
+        Ax[:, :, end] .= -Ax[:, :, end - 1]
+        Ay[:, :, end] .= -Ay[:, :, end - 1]
+        Az[:, :, end] .= 0
+    end
 end

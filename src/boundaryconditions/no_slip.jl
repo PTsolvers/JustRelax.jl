cf8f4d5128718671ba9e9bab368df94e6526fd4b
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-07-04 18:57:59 +0200

    fix

--- src/boundaryconditions/no_slip.jl
+++ src/boundaryconditions/no_slip.jl
@@ -1,4 +1,4 @@
-@parallel_indices (i) function no_slip1!(Ax, Ay, bc)
+@parallel_indices (i) function no_slip2!(Ax, Ay, bc)
     @inbounds begin
         if bc.left
             (1 < i < size(Ay, 2)) && (Ay[1, i] = -Ay[2, i])
@@ -16,7 +16,7 @@
     return nothing
 end
 
-@parallel_indices (i) function no_slip2!(Ax, Ay, bc)
+@parallel_indices (i) function no_slip1!(Ax, Ay, bc)
     @inbounds begin
         if bc.left
             (i ≤ size(Ax, 2)) && (Ax[1, i] = 0.0)
@@ -34,40 +34,40 @@ end
     return nothing
 end
 
-@parallel_indices (i, j) function no_slip1!(Ax, Ay, Az, bc)
-    @inbounds begin
+@parallel_indices (i, j) function no_slip2!(Ax, Ay, Az, bc)
+    # @inbounds begin
         if bc.left
-            (1 ≤ size(Ay, 2)) && (1 ≤ size(Ay, 3)) && (Ay[1, i, j] = -Ay[2, i, j])
-            (1 ≤ size(Az, 2)) && (1 ≤ size(Az, 3)) && (Az[1, i, j] = -Az[2, i, j])
+            (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[1, i, j] = -Ay[2, i, j])
+            (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[1, i, j] = -Az[2, i, j])
         end
         if bc.right
-            (1 ≤ size(Ay, 2)) && (1 ≤ size(Ay, 3)) && (Ay[end, i, j] = -Ay[end - 1, i, j])
-            (1 ≤ size(Az, 2)) && (1 ≤ size(Az, 3)) && (Az[end, i, j] = -Az[end - 1, i, j])
+            (i ≤ size(Ay, 2)) && (j ≤ size(Ay, 3)) && (Ay[end, i, j] = -Ay[end - 1, i, j])
+            (i ≤ size(Az, 2)) && (j ≤ size(Az, 3)) && (Az[end, i, j] = -Az[end - 1, i, j])
         end
 
         if bc.front
-            (1 ≤ size(Ax, 1)) && (1 ≤ size(Ax, 3)) && (Ax[i, 1, j] = -Ax[i, 2, j])
-            (1 ≤ size(Az, 1)) && (1 ≤ size(Az, 3)) && (Az[i, 1, j] = -Az[i, 2, j])
+            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, 1, j] = -Ax[i, 2, j])
+            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, 1, j] = -Az[i, 2, j])
         end
         if bc.back
-            (1 ≤ size(Ax, 1)) && (1 ≤ size(Ax, 3)) && (Ax[i, end, j] = -Ax[i, end - 1, j])
-            (1 ≤ size(Az, 1)) && (1 ≤ size(Az, 3)) && (Az[i, end, j] = -Az[i, end - 1, j])
+            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 3)) && (Ax[i, end, j] = -Ax[i, end - 1, j])
+            (i ≤ size(Az, 1)) && (j ≤ size(Az, 3)) && (Az[i, end, j] = -Az[i, end - 1, j])
         end
 
         if bc.bot
-            (1 ≤ size(Ax, 1)) && (1 ≤ size(Ax, 2)) && (Ax[i, j, 1] = -Ax[i, j, 2])
-            (1 ≤ size(Ay, 1)) && (1 ≤ size(Ay, 2)) && (Ay[i, j, 1] = -Ay[i, j, 2])
+            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, 1] = -Ax[i, j, 2])
+            (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, 1] = -Ay[i, j, 2])
         end
         if bc.top
-            (1 ≤ size(Ax, 1)) && (1 ≤ size(Ax, 2)) && (Ax[i, j, end] = -Ax[i, j, end - 1])
-            (1 ≤ size(Ay, 1)) && (1 ≤ size(Ay, 2)) && (Ay[i, j, end] = -Ay[i, j, end - 1])
+            (i ≤ size(Ax, 1)) && (j ≤ size(Ax, 2)) && (Ax[i, j, end] = -Ax[i, j, end - 1])
+            (i ≤ size(Ay, 1)) && (j ≤ size(Ay, 2)) && (Ay[i, j, end] = -Ay[i, j, end - 1])
         end
-    end
+    # end
     return nothing
 end
 
-@parallel_indices (i, j) function no_slip2!(Ax, Ay, Az, bc)
-    @inbounds begin
+@parallel_indices (i, j) function no_slip1!(Ax, Ay, Az, bc)
+    # @inbounds begin
         ##
         if bc.left
             (i ≤ size(Ax, 2)) && (j ≤ size(Ax, 3)) && (Ax[1, i, j] = 0.0)
@@ -89,6 +89,6 @@ end
         if bc.top
             (i ≤ size(Az, 1)) && (j ≤ size(Az, 2)) && (Az[i, j, end] = 0.0)
         end
-    end
+    # end
     return nothing
 end

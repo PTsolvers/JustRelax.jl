0315f5f695cb528f41ec7c0610cbbfee48e54fed
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-07-08 23:28:02 +0200

    khefalonia

---
+++ KhefaloniaRheology.jl
@@ -0,0 +1,125 @@
+# from "Fingerprinting secondary mantle plumes", Cloetingh et al. 2022
+
+function init_rheologies(; is_plastic = true)
+
+    # Dislocation and Diffusion creep
+    disl_upper_crust            = DislocationCreep(A=5.07e-18, n=2.3, E=154e3, V=6e-6,  r=0.0, R=8.3145)
+    disl_lower_crust            = DislocationCreep(A=2.08e-23, n=3.2, E=238e3, V=6e-6,  r=0.0, R=8.3145)
+    # disl_lithospheric_mantle    = DislocationCreep(A=2.51e-17, n=3.5, E=530e3, V=6e-6,  r=0.0, R=8.3145)
+    # disl_sublithospheric_mantle = DislocationCreep(A=2.51e-17, n=3.5, E=530e3, V=6e-6,  r=0.0, R=8.3145)
+    # diff_lithospheric_mantle    = DiffusionCreep(A=2.51e-17, n=1.0, E=530e3, V=6e-6,  p=0, r=0.0, R=8.3145)
+    # diff_sublithospheric_mantle = DiffusionCreep(A=2.51e-17, n=1.0, E=530e3, V=6e-6,  p=0, r=0.0, R=8.3145)
+
+    # Elasticity
+    el_upper_crust              = SetConstantElasticity(; G=25e9, ν=0.5)
+    el_lower_crust              = SetConstantElasticity(; G=25e9, ν=0.5)
+    # el_lithospheric_mantle      = SetConstantElasticity(; G=67e9, ν=0.5)
+    # el_sublithospheric_mantle   = SetConstantElasticity(; G=67e9, ν=0.5)
+    β_upper_crust               = inv(get_Kb(el_upper_crust))
+    β_lower_crust               = inv(get_Kb(el_lower_crust))
+    # β_lithospheric_mantle       = inv(get_Kb(el_lithospheric_mantle))
+    # β_sublithospheric_mantle    = inv(get_Kb(el_sublithospheric_mantle))
+
+    # Physical properties using GeoParams ----------------
+    η_reg     = 1e16
+    cohesion  = 3e6
+    friction  = asind(0.2)
+    pl_crust  = DruckerPrager_regularised(; C = cohesion, ϕ=friction, η_vp=η_reg, Ψ=0.0) # non-regularized plasticity
+    
+    # crust
+    K_crust = TP_Conductivity(;
+        a = 0.64,
+        b = 807e0,
+        c = 0.77,
+        d = 0.00004*1e-6,
+    )
+
+    # Define rheolgy struct
+    rheology = (
+        # Name              = "UpperCrust",
+        SetMaterialParams(;
+            Phase             = 1,
+            Density           = PT_Density(; ρ0=2.75e3, β=β_upper_crust, T0=0.0, α = 3.5e-5),
+            HeatCapacity      = ConstantHeatCapacity(; Cp=1e3),
+            Conductivity      = ConstantConductivity(; k=3.0),
+            # Conductivity      = K_crust,
+            CompositeRheology = CompositeRheology((disl_upper_crust, el_upper_crust, pl_crust)),
+            Elasticity        = el_upper_crust,
+            RadioactiveHeat   = ConstantRadioactiveHeat(0.0),
+            Gravity           = ConstantGravity(; g=9.81),
+        ),
+        # Name              = "LowerCrust",
+        SetMaterialParams(;
+            Phase             = 2,
+            Density           = PT_Density(; ρ0=3e3, β=β_lower_crust, T0=0.0, α = 3.5e-5),
+            HeatCapacity      = ConstantHeatCapacity(; Cp=1e3),
+            Conductivity      = ConstantConductivity(; k=3.0),
+            # Conductivity      = K_crust,
+            RadioactiveHeat   = ConstantRadioactiveHeat(0.0),
+            CompositeRheology = CompositeRheology((disl_lower_crust, el_lower_crust, pl_crust)),
+            Elasticity        = el_lower_crust,
+        ),
+        # Name              = "StickyAir",
+        SetMaterialParams(;
+            Phase             = 3,
+            Density           = ConstantDensity(; ρ=1e2), # water density
+            HeatCapacity      = ConstantHeatCapacity(; Cp=7.5e2),
+            RadioactiveHeat   = ConstantRadioactiveHeat(0.0),
+            # Conductivity      = ConstantConductivity(; k=1e-2),
+            Conductivity      = ConstantConductivity(; k=50),
+            CompositeRheology = CompositeRheology((LinearViscous(; η=1e19),)),
+        ),
+        # Name              = "Watah",
+        SetMaterialParams(;
+            Phase             = 4,
+            Density           = ConstantDensity(; ρ=1e3), # water density
+            HeatCapacity      = ConstantHeatCapacity(; Cp=7.5e2),
+            RadioactiveHeat   = ConstantRadioactiveHeat(0.0),
+            # Conductivity      = ConstantConductivity(; k=1e-2),
+            Conductivity      = ConstantConductivity(; k=50),
+            CompositeRheology = CompositeRheology((LinearViscous(; η=1e19),)),
+        ),
+    )
+end
+
+
+function init_phases!(phases, phase_grid, particles, xvi)
+    ni = size(phases)
+    @parallel (@idx ni) _init_phases!(phases, phase_grid, particles.coords, particles.index, xvi)
+end
+
+@parallel_indices (I...) function _init_phases!(phases, phase_grid, pcoords::NTuple{N, T}, index, xvi) where {N,T}
+
+    ni = size(phases)
+
+    for ip in JustRelax.cellaxes(phases)
+        # quick escape
+        @cell(index[ip, I...]) == 0 && continue
+
+        pᵢ = ntuple(Val(N)) do i 
+            @cell pcoords[i][ip, I...]
+        end
+
+        d = Inf # distance to the nearest particle
+        particle_phase = -1
+        for offi in 0:1, offj in 0:1
+            ii, jj = I[1] + offi, I[2] + offj
+
+            !(ii ≤ ni[1]) && continue
+            !(jj ≤ ni[2]) && continue
+
+            xvᵢ = (
+                xvi[1][ii], 
+                xvi[2][jj], 
+            )
+            d_ijk = √(sum((pᵢ[i] - xvᵢ[i])^2 for i in 1:N))
+            if d_ijk < d
+                d = d_ijk
+                particle_phase = phase_grid[ii, jj]
+            end
+        end
+        JustRelax.@cell phases[ip, I...] = Float64(particle_phase)
+    end
+
+    return nothing
+end
---
+++ KhefaloniaSetup.jl
@@ -0,0 +1,93 @@
+using GLMakie
+using GeophysicalModelGenerator, GMT
+using Interpolations
+
+function Khefalonia_topo(N)
+    # import topo from GMT's server
+    Topo = import_topo([20.2, 20.8, 38.3, 38.5], file="@earth_relief_03s")
+    # Topo = import_topo([20, 21, 38.3, 38.5], file="@earth_relief_03s")
+    # extract 2D array with the topographic elevation
+    surf = Topo.depth.val[:,:,1]
+    l = maximum(abs.(Topo.depth.val))
+    heatmap(surf, colorrange=(-l,l), colormap=:oleron )
+    # GLMakie.surface(surf, colorrange=(-l,l), colormap=:oleron )
+
+    # our model is in [km] -> need to convert from lat-long
+    x = LinRange(20.4, 20.8, N) |> collect
+    y = LinRange(38.3, 38.5, N) |> collect
+    X = (
+        Topo.lon.val[:, 1, 1],
+        Topo.lat.val[1, :, 1],
+    )
+
+    # interpolate from GMT's surface to the resolution nx × ny resolution of our grid
+    itp = interpolate(X, surf, Gridded(Linear()))
+    surf_interp = [itp(x, y) for x in x, y in y]
+
+    # compute the x and y size of our cartesian model
+    lat_dist  = extrema(X[1]) |> collect |> diff |> first |> abs
+    long_dist = extrema(X[2]) |> collect |> diff |> first |> abs
+    Lx        = lat_dist * 110.574
+    Ly        = long_dist * 111.320*cos(lat_dist)
+    
+    return surf_interp, Lx, Ly, surf_interp[:, 30]
+end
+
+function Khefalonia__setup(N)
+    surf, Lx, Ly, topo = Khefalonia_topo(N)
+
+    nx = nz = N 
+    ny = 2
+    x = range(-Lx / 2, Lx / 2, nx);
+    y = range(-Ly / 2, Ly / 2, ny);
+    z = range(-25, 5, nz);
+    Grid = CartData(xyz_grid(x,y,z));
+
+    # Now we create an integer array that will hold the `Phases` information (which usually refers to the material or rock type in the simulation)
+    Phases = fill(1, nx, ny, nz);
+
+    # In many (geodynamic) models, one also has to define the temperature, so lets define it as well
+    Temp = fill(20.0, nx, ny, nz);
+
+    lith = LithosphericPhases(Layers=[15 45 100], Phases=[1 2 3])
+
+    add_box!(Phases, Temp, Grid; 
+        xlim=(-Lx, Lx), 
+        ylim=(-Ly, Ly), 
+        zlim=(-60.0, 0.0), 
+        phase = lith, 
+        T = HalfspaceCoolingTemp(Age=20)
+    )
+   
+    # add_ellipsoid!(Phases, Temp, Grid; 
+    #     cen    = (0, 0, -12.5), 
+    #     axes   = (5, 5, 2.5),
+    #     phase  = ConstantPhase(3),
+    # )
+
+    # id = falses(nx, ny, nz)
+    for k in axes(Grid.z.val, 3), j in axes(topo, 2), i in axes(topo,1)
+        # id[i, j, k] = Grid.z.val[i, j, k] > topo_volcano[i, j]
+        if Grid.z.val[i, j, k] > topo[i]
+            if Grid.z.val[i, j, k]> 0 
+                Phases[i, j, k] = 3 # air
+            else
+                Phases[i, j, k] = 4 # water
+            end
+            Temp[i, j, k] = 20
+        end
+    end
+
+    # @. Temp[Phases == 3]  = 20
+    # @. Temp[Phases == 4]  = 20 
+    # @. Temp               = max(Temp, 20)
+    Grid = addfield(Grid,(; Phases, Temp))
+
+    li     = abs(last(x)-first(x)),  abs(last(y)-first(y))
+    origin = x[1], y[1]
+
+    ph      = Phases
+    T       = Temp
+
+    return li, origin, ph[:,1,:], T[:,1,:] .+ 273
+end

d0eb026cb2d20f4b7d7daf7801d063f85d69f72b
Pascal Aellig <93140290+aelligp@users.noreply.github.com>
2024-08-28 10:15:55 +0200

    Thermal Stresses with `MeltDependent_Density` (#225)
    * update thermal expansion for meltviscosity
    
    * add `get_α` for `MeltDependent_Density`
    
    * format
    
    * format again

--- src/rheology/GeoParams.jl
+++ src/rheology/GeoParams.jl
@@ -16,6 +16,15 @@ end
 
 get_thermal_expansion(args::Vararg{Any,N}) where {N} = get_α(args...)
 
+function get_α(rho::MeltDependent_Density; ϕ::T=0.0, kwargs...) where {T}
+    αsolid = get_α(rho.ρsolid)
+    αmelt = get_α(rho.ρmelt)
+    return ϕ * αmelt + (1 - ϕ) * αsolid
+end
+
 @inline get_α(p::MaterialParams) = get_α(p.Density[1])
-@inline get_α(p::ConstantDensity) = 0.0
+@inline get_α(p::MaterialParams, args::NamedTuple) = get_α(p.Density[1], args)
 @inline get_α(p::Union{T_Density,PT_Density}) = GeoParams.get_α(p)
+@inline get_α(rho::MeltDependent_Density, args) = get_α(rho; args...)
+@inline get_α(rho::ConstantDensity, args) = 0
+@inline get_α(rho::ConstantDensity) = 0

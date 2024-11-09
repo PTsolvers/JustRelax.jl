4a6460e8f8f9350e3658800778b5d2744ae96e30
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-11-09 19:03:00 +0100

    format

--- src/boundaryconditions/BoundaryConditions.jl
+++ src/boundaryconditions/BoundaryConditions.jl
@@ -4,8 +4,9 @@ include("free_surface.jl")
 include("no_slip.jl")
 include("pure_shear.jl")
 
-@inline bc_index(x::T) where {T<:AbstractArray{_T, 2} where _T} = max(size(x)...)
-@inline bc_index(x::T) where {T<:AbstractArray{_T, 3} where _T} = max(size(x)...), max(size(x)...)
+@inline bc_index(x::T) where {T<:AbstractArray{_T,2} where {_T}} = max(size(x)...)
+@inline bc_index(x::T) where {T<:AbstractArray{_T,3} where {_T}} =
+    max(size(x)...), max(size(x)...)
 
 @inline bc_index(x::NTuple{2,T}) where {T} = mapreduce(xi -> max(size(xi)...), max, x)
 @inline function bc_index(x::NTuple{3,T}) where {T}

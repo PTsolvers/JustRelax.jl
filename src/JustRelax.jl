d5804e2fc0ee2bb3c6c4e9b33a7ffe4edf7f9653
Pascal Aellig <93140290+aelligp@users.noreply.github.com>
2025-01-23 15:32:34 +0100

    add versioning to startup message

--- src/JustRelax.jl
+++ src/JustRelax.jl
@@ -10,19 +10,23 @@ using HDF5
 using CellArrays
 using StaticArrays
 using Statistics
+using TOML
 @reexport using JustPIC
 
 function solve!() end
 #! format: off
 function __init__()
-    println("""
+    printstyled("""
          _           _   ____      _               _ _
         | |_   _ ___| |_|  _ \\ ___| | __ ___  __  (_) |
      _  | | | | / __| __| |_) / _ \\ |/ _` \\ \\/ /  | | |
     | |_| | |_| \\__ \\ |_|  _ <  __/ | (_| |>  < _ | | |
      \\___/ \\__,_|___/\\__|_| \\_\\___|_|\\__,_/_/\\_(_)/ |_|
                                                 |__/
-    """)
+    Version: $(TOML.parsefile("Project.toml")["version"])
+    Latest commit: $(strip(read(`git log -1 --pretty=%B`, String)))
+    Commit date: $(strip(read(`git log -1 --pretty=%cd`, String)))
+""", bold=true, color=:white)
 end
 #! format: on
 abstract type AbstractBackend end

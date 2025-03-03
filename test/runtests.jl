ffd701aa08391e1f04da08b5c704c1ad1b32d9da
Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2025-03-03 11:21:12 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -73,7 +73,7 @@ function runtests()
     return nfail
 end
 
-_, backend_name = parse_flags!(ARGS, "--backend"; default="CPU", type = String)
+_, backend_name = parse_flags!(ARGS, "--backend"; default = "CPU", type = String)
 
 @static if backend_name == "AMDGPU"
     Pkg.add("AMDGPU")

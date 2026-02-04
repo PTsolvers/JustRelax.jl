365f0e0da45b13d9bd8e1b369f2acc9a787a3780
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-02-04 11:26:28 +0100

    format

--- src/JustRelax_CPU.jl
+++ src/JustRelax_CPU.jl
@@ -60,7 +60,7 @@ module JustRelax3D
         apply_dirichlet,
         apply_dirichlet!,
         isdirichlet
-        
+
     import JustRelax: normal_stress, shear_stress, shear_vorticity
 
     import JustPIC._3D: numphases, nphases, PhaseRatios, update_phase_ratios!, compute_dx, face_offset

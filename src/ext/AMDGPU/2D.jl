bfefcdad52b04759eef2e5e528f288f031dc3a6a
Pascal Aellig <pascal.aellig@hotmail.com>
2026-04-29 15:54:04 +0200

    fix AMDGPU ext

--- src/ext/AMDGPU/2D.jl
+++ src/ext/AMDGPU/2D.jl
@@ -149,6 +149,25 @@ function JR2D.update_thermal_coeffs!(
     return nothing
 end
 
+function JR2D.PrincipalStress(backend::Type{AMDGPUBackend}, ni::NTuple{N, Integer}) where {N}
+    return PrincipalStress(ni)
+end
+
+function JR2D.compute_principal_stresses(backend::Type{AMDGPUBackend}, stokes::JustRelax.StokesArrays)
+    ni = size(stokes.P)
+    σ = JR2D.PrincipalStress(backend, ni)
+    compute_principal_stresses!(stokes, σ)
+    return σ
+end
+
+
+function JR2D.compute_principal_stresses!(stokes, σ::JustRelax.PrincipalStress{<:ROCArray})
+    ni = size(stokes.P)
+    @parallel (@idx ni) principal_stresses_eigen!(σ, @stress_center(stokes)...)
+    return nothing
+end
+
+
 # Boundary conditions
 function JR2D.flow_bcs!(
         ::AMDGPUBackendTrait, stokes::JustRelax.StokesArrays, bcs::VelocityBoundaryConditions

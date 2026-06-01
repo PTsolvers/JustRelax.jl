928dd0dd61816c4277ec843013ba1413aac0f642
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-06-01 10:04:58 +0200

    fix T setup with GMG

--- miniapps/subduction/2D/Subduction2D.jl
+++ miniapps/subduction/2D/Subduction2D.jl
@@ -116,8 +116,6 @@ function main(li, origin, phases_GMG, igg; nx = 16, ny = 16, figdir = "figs2D", 
         constant_value = (left = false, right = false, top = Ttop, bot = Tbot),
     )
     thermal_bcs!(thermal, thermal_bc)
-    @views thermal.T[:, end - 1] .= Ttop
-    @views thermal.T[:, 2] .= Tbot
     # ----------------------------------------------------
 
     # Buoyancy forces

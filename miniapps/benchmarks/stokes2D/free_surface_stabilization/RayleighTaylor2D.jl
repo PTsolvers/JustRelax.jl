1d4bdfa97a224b84bdd6cc04371bffcbf7807625
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-08-25 09:09:37 +0200

    fix miniapp

--- miniapps/benchmarks/stokes2D/free_surface_stabilization/RayleighTaylor2D.jl
+++ miniapps/benchmarks/stokes2D/free_surface_stabilization/RayleighTaylor2D.jl
@@ -30,33 +30,7 @@ else
 end
 
 # Load script dependencies
-# const backend = CPUBackend
-
-# const backend_JP = JustPIC.CPU
-
-
-using LinearAlgebra, GeoParams, CairoMakie
-
-# Velocity helper grids for the particle advection
-function copyinn_x!(A, B)
-    @parallel function f_x(A, B)
-        @all(A) = @inn_x(B)
-        return nothing
-    end
-    return @parallel f_x(A, B)
-end
-
-import ParallelStencil.INDICES
-const idx_j = INDICES[2]
-macro all_j(A)
-    return esc(:($A[$idx_j]))
-end
-
-# Initial pressure profile - not accurate
-@parallel function init_P!(P, ρg, z)
-    @all(P) = abs(@all(ρg) * @all_j(z)) * <(@all_j(z), 0.0)
-    return nothing
-end
+using GeoParams, CairoMakie
 
 function init_phases!(phases, particles, A)
     ni = size(phases)
@@ -156,15 +130,10 @@ function main(igg, nx, ny)
     update_phase_ratios!(phase_ratios, particles, pPhases)
     # ----------------------------------------------------
 
-    # RockRatios
-    air_phase = 1
-    ϕ = RockRatio(backend, ni)
-    update_rock_ratio!(ϕ, phase_ratios, air_phase)
-
     # STOKES ---------------------------------------------
     # Allocate arrays needed for every Stokes problem
     stokes = StokesArrays(backend, ni)
-    pt_stokes = PTStokesCoeffs(li, di; ϵ_abs = 1.0e-6, ϵ_rel = 1.0e-4, Re = 3.0e0, r = 0.7, CFL = 0.98 / √2.1)
+    pt_stokes = PTStokesCoeffs(li, di; ϵ_abs = 1.0e-6, ϵ_rel = 1.0e-4, Re = 6.0e0, r = 0.7, CFL = 0.98 / √2.1)
     # ----------------------------------------------------
 
     # TEMPERATURE PROFILE --------------------------------
@@ -175,14 +144,14 @@ function main(igg, nx, ny)
     ρg = @zeros(ni...), @zeros(ni...)
     args = (; T = thermal.T, P = stokes.P, dt = Inf)
     compute_ρg!(ρg[2], phase_ratios, rheology, args)
-    @parallel init_P!(stokes.P, ρg[2], xci[2])
+    compute_lithostatic_pressure!(stokes.P, ρg[2], di[2], igg)
     compute_viscosity!(stokes, phase_ratios, args, rheology, (-Inf, Inf))
 
     # Boundary conditions
     flow_bcs = VelocityBoundaryConditions(;
         free_slip = (left = true, right = true, top = true, bot = false),
         no_slip = (left = false, right = false, top = false, bot = true),
-        free_surface = true,
+        free_surface = false,
     )
 
     Vx_v = @zeros(ni .+ 1...)
@@ -196,41 +165,30 @@ function main(igg, nx, ny)
     dt = 10.0e3 * (3600 * 24 * 365.25)
     dt_max = 10.0e3 * (3600 * 24 * 365.25)
 
-    _di = inv.(di)
-    (; ϵ_rel, r, θ_dτ, ηdτ) = pt_stokes
-    (; η, η_vep) = stokes.viscosity
-    ni = size(stokes.P)
-    iterMax = 15.0e3
-    nout = 1.0e3
-    viscosity_cutoff = (-Inf, Inf)
-    free_surface = false
-    ητ = @zeros(ni...)
-    while it < 1000
+    while it < 1#000
 
-        ## variational solver
+        # Use one physical timestep for stabilization, advection, and elapsed time.
+        dt = compute_dt(stokes, di, dt_max)
+
         # Stokes solver ----------------
-        solve_VariationalStokes!(
+        solve!(
             stokes,
             pt_stokes,
             grid,
             flow_bcs,
             ρg,
             phase_ratios,
-            ϕ,
             rheology,
             args,
             dt,
             igg;
-            kwargs = (
+            kwargs = (;
                 iterMax = 50.0e3,
-                iterMin = 1.0e3,
-                viscosity_relaxation = 1.0e-2,
-                nout = 2.0e3,
-                free_surface = true,
+                nout = 1.0e3,
                 viscosity_cutoff = (-Inf, Inf),
+                free_surface = true,
             )
         )
-        dt = compute_dt(stokes, di, dt_max)
         # ------------------------------
 
         # Advection --------------------
@@ -242,7 +200,6 @@ function main(igg, nx, ny)
         inject_particles_phase!(particles, pPhases, (), ())
         # update phase ratios
         update_phase_ratios!(phase_ratios, particles, pPhases)
-        update_rock_ratio!(ϕ, phase_ratios, air_phase)
 
         @show it += 1
         t += dt

025bbaa3359cbeb34c2a1b343f070fe398b414cc
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-09-16 08:44:08 +0200

    last test fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,6 +48,7 @@ function runtests(args)
     if backend_name != "CPU"
         delete!(testsuite, "test_variational_operators_2D")
         delete!(testsuite, "test_rheology")
+        delete!(testsuite, "test_dyrel_solver_3D")
     end
 
     # Separate MPI tests – always run sequentially via mpiexec

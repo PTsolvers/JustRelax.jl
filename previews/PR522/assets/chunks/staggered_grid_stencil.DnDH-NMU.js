e64fe4db5f8c5389e536deef546f2fdcaca623ec
Documenter.jl <documenter@juliadocs.github.io>
2026-09-17 08:56:51 +0000

    delete history

---
+++ previews/PR522/assets/chunks/staggered_grid_stencil.DnDH-NMU.js
@@ -0,0 +1,1 @@
+const s="/JustRelax.jl/previews/PR522/assets/staggered_grid_stencil.DTY7TrGC.png";export{s as _};
---
+++ stable/assets/chunks/stokes_stag2D.D2gANkVm.js
@@ -0,0 +1,1 @@
+const s="/JustRelax.jl/stable/assets/temp_stag2D.CPrcX1ke.png",t="/JustRelax.jl/stable/assets/stokes_stag2D.5cFNPKDo.png";export{s as _,t as a};
